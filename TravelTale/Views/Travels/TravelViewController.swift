c2133aedf50b66b352d2f6fa98a335689c2ada35
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-06-05 00:10:14 +0900

    TravelViewController 설정

--- TravelTale/Views/Travels/TravelViewController.swift
+++ TravelTale/Views/Travels/TravelViewController.swift
@@ -8,22 +8,19 @@
 import UIKit
 
 class TravelViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    let travelView = TravelView()
+    
+    override func loadView() {
+        view = travelView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        travelView.addButtonLabel.text = "새 여행 추가"
     }
-    */
+    
+    
 
 }
