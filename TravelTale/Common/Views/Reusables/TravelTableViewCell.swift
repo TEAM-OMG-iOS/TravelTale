fc3267d88c060d57eb9a5cbafdc24b3400f6bb47
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-06-05 19:47:31 +0900

    #1 - TravelTableViewCell 설정

--- TravelTale/Common/Views/Reusables/TravelTableViewCell.swift
+++ TravelTale/Common/Views/Reusables/TravelTableViewCell.swift
@@ -11,31 +11,32 @@ class TravelTableViewCell: UITableViewCell {
     
     // MARK: - Properties
     
+    static let identifier = String(describing: TravelTableViewCell.self)
+    
     let thumbnailImageView = UIImageView()
     let borderLine = UIView()
     let periodLabel = UILabel()
     let provinceCapsuleView = UIView()
-    let provinceTitleLabel = UILabel()
-    let detailStackView = UIStackView()
+    let provinceNameLabel = UILabel()
     let titleLabel = UILabel()
     
     // MARK: - Life Cycles
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    // MARK: - Methods
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
         configureUI()
         configureHierarchy()
         configureConstraints()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
-    // MARK: - Methods
     
-    func configureUI() {
-        self.backgroundColor = .white
+    private func configureUI() {
         
         contentView.layer.cornerRadius = 15
         contentView.layer.borderWidth = 1
@@ -43,61 +44,63 @@ class TravelTableViewCell: UITableViewCell {
         
         thumbnailImageView.layer.cornerRadius = 16
         thumbnailImageView.contentMode = .scaleAspectFill
-        thumbnailImageView.snp.makeConstraints {
-            $0.size.equalTo(64)
-        }
         
         borderLine.backgroundColor = .gray20
-        borderLine.snp.makeConstraints {
-            $0.height.equalTo(1)
-        }
         
         periodLabel.font = .oaGothic(size: 10, weight: .medium)
         periodLabel.textColor = .gray90
         
         provinceCapsuleView.backgroundColor = .blueBlack100
-        provinceCapsuleView.snp.makeConstraints {
-            $0.width.equalTo(38)
-            $0.height.equalTo(22)
-        }
+        provinceCapsuleView.layer.cornerRadius = 10
         
-        provinceTitleLabel.font = .pretendard(size: 12, weight: .medium)
-        provinceTitleLabel.textColor = .white
+        provinceNameLabel.font = .pretendard(size: 12, weight: .medium)
+        provinceNameLabel.textColor = .white
+        provinceNameLabel.textAlignment = .center
         
         titleLabel.font = .pretendard(size: 18, weight: .medium)
         titleLabel.textColor = .black
         
     }
     
-    func configureHierarchy() {
+    private func configureHierarchy() {
         
         [thumbnailImageView,
          borderLine,
-         detailStackView,
+         periodLabel,
+         provinceCapsuleView,
          titleLabel].forEach { contentView.addSubview($0) }
         
-        [periodLabel,
-         provinceCapsuleView].forEach { detailStackView.addArrangedSubview($0) }
-        
-        provinceCapsuleView.addSubview(provinceTitleLabel)
+        provinceCapsuleView.addSubview(provinceNameLabel)
     }
     
-    func configureConstraints() {
+    private func configureConstraints() {
         
         thumbnailImageView.snp.makeConstraints {
+            $0.size.equalTo(64)
             $0.leading.verticalEdges.equalToSuperview().inset(16)
         }
         
         borderLine.snp.makeConstraints {
+            $0.height.equalTo(1)
             $0.leading.equalTo(thumbnailImageView.snp.trailing).offset(12)
             $0.trailing.equalToSuperview().inset(16)
             $0.centerY.equalToSuperview()
         }
         
-        detailStackView.snp.makeConstraints {
+        periodLabel.snp.makeConstraints {
+            $0.centerY.equalTo(provinceCapsuleView)
+            $0.leading.equalTo(borderLine)
+        }
+        
+        provinceCapsuleView.snp.makeConstraints {
             $0.height.equalTo(22)
-            $0.bottom.equalTo(borderLine.snp.top).offset(8)
-            $0.horizontalEdges.equalTo(borderLine)
+            $0.width.equalTo(38)
+            $0.bottom.equalTo(borderLine.snp.top).offset(-8)
+            $0.trailing.equalTo(borderLine)
+        }
+        
+        provinceNameLabel.snp.makeConstraints{
+            $0.center.equalToSuperview()
         }
         
         titleLabel.snp.makeConstraints {
@@ -105,6 +108,25 @@ class TravelTableViewCell: UITableViewCell {
             $0.top.equalTo(borderLine.snp.bottom).offset(8)
             $0.horizontalEdges.equalTo(borderLine)
         }
+    }
+    
+    func bind(image: Data?, title: String?, period: String?, province: String?) {
+        
+        if let image = image {
+            thumbnailImageView.image = UIImage(data: image)
+        } else {
+            thumbnailImageView.backgroundColor = .gray20
+            thumbnailImageView.tintColor = .white
+//            thumbnailImageView.image = UIImage(systemName: "photo")
+//            thumbnailImageView.contentMode = .scaleAspectFit
+        }
+        
+        titleLabel.text = title ?? "여행 이름을 불러올 수 없습니다."
+        
+        periodLabel.text = period ?? "여행 기간을 불러올 수 없습니다."
+        
+        provinceNameLabel.text = province ?? "미정"
+        
     }
     
 }
---
+++ TravelTale/Views/Travels/TravelViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TravelViewModel.swift
+//  TravelTale
+//
+//  Created by 유림 on 6/5/24.
+//
+
+import Foundation
