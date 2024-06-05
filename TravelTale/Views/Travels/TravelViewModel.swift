501ed8b50ab2b7e6eb9105995ff4c54ab3b27b5e
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-06-05 19:49:33 +0900

    #1 - TravelViewModel 설정

--- TravelTale/Views/Travels/TravelViewModel.swift
+++ TravelTale/Views/Travels/TravelViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+// ------------------------------------
+// 임시 데이터 구조
+struct Travel {
+    let image: Data?
+    let title: String
+    let startDate: Date
+    let endDate: Date
+    let province: String?
+}
+// ------------------------------------
+
+class TravelViewModel {
+    
+    var travelArray: Observable<[Travel]> = .init([])
+    
+    func returnPeriodString(startDate: Date, endDate: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy.MM.dd"
+        
+        let startDateString = dateFormatter.string(from: startDate)
+        let endDateString = dateFormatter.string(from: endDate)
+        
+        return "\(startDateString) ~ \(endDateString)"
+    }
+    
+}
