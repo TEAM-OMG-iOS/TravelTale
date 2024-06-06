1ef60abd5f7603145b759d906a99f32cd9025a2d
Bread-kn72 <ordinarytom72@gmail.com>
2024-06-06 21:36:40 +0900

    [Design] (TravelAdditionViewController) 초기 뷰구현

--- TravelTale/Common/Views/Reusables/TravelAdditionViewController.swift
+++ TravelTale/Common/Views/Reusables/TravelAdditionViewController.swift
@@ -5,4 +5,60 @@
 //  Created by Kinam on 6/4/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class TravelAdditionViewController: BaseViewController {
+    // MARK: - properties
+    let travelAdditionView1 = TravelAdditionView1()
+    
+    
+    // MARK: - life cycles
+    override func loadView() {
+        super.loadView()
+        view = travelAdditionView1
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureStyle()
+        configureDelegate()
+        configureAddTarget()
+        bind()
+        configureNavigationBar()
+        travelAdditionView1.startLoadingAnimation()
+    }
+    
+    
+    
+    // MARK: - methods
+    override func configureStyle() { }
+    
+    override func configureDelegate() { }
+    
+    override func configureAddTarget() { }
+    
+    override func bind() { }
+    
+    private func configureNavigationBar() {
+        navigationItem.title = "내 여행에 추가"
+        
+        let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: self, action: #selector(handleBackButton))
+        navigationItem.leftBarButtonItem = backButton
+        navigationItem.leftBarButtonItem?.tintColor = .black
+        
+        if let navigationBar = self.navigationController?.navigationBar {
+            navigationBar.titleTextAttributes = [
+                NSAttributedString.Key.font: UIFont.oaGothic(size: 18, weight: .heavy),
+                NSAttributedString.Key.foregroundColor: UIColor.black
+            ]
+        }
+    }
+    
+    @objc private func handleBackButton() {
+            navigationController?.popViewController(animated: true)
+        }
+    
+    
+    // MARK: - extensions
+}
