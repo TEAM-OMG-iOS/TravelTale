a8886eb4578da2614870de0cde18fa1cbd483a23
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-06-08 01:22:20 +0900

    [Design] #1 - (TravelMemoryView) UI 구성

---
+++ TravelTale/Views/Travels/Travel/TravelMemoryView.swift
@@ -0,0 +1,43 @@
+//
+//  TravelMemoryView.swift
+//  TravelTale
+//
+//  Created by 유림 on 6/8/24.
+//
+
+import UIKit
+
+class TravelMemoryView: BaseView {
+    
+    // MARK: - properties
+    let addView = TravelAddView()
+    let tableView = UITableView()
+    
+    
+    // MARK: - methods
+    override func configureUI() {
+        super.configureUI()
+        addView.label.text = "추억 남기기"
+    }
+    
+    override func configureHierarchy() {
+        [addView,
+         tableView].forEach { self.addSubview($0) }
+    }
+    
+    override func configureConstraints() {
+        
+        let horizontalInset = 20
+        
+        addView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(26)
+            $0.horizontalEdges.equalToSuperview().inset(horizontalInset)
+        }
+        
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(addView.snp.bottom).offset(26)
+            $0.horizontalEdges.equalToSuperview().inset(horizontalInset)
+            $0.bottom.equalToSuperview()
+        }
+    }
+}
---
+++ TravelTale/Views/Travels/TravelSegment.swift
@@ -0,0 +1,8 @@
+//
+//  TravelSegment.swift
+//  TravelTale
+//
+//  Created by 유림 on 6/8/24.
+//
+
+import Foundation
