1b110031cb901945c94dbbf8090f17623f222905
KJH <gh7052@gmail.com>
2024-06-08 01:39:55 +0900

    [Feat] #8 - (TravelPlanDetailsViewController) 초기 기능 구성

--- TravelTale/Views/Travels/TravelPlanDetailsViewController.swift
+++ TravelTale/Views/Travels/TravelPlanDetailsViewController.swift
@@ -7,23 +7,55 @@
 
 import UIKit
 
-class TravelPlanDetailsViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+final class TravelPlanDetailsViewController: BaseViewController {
+    
+    // MARK: - properties
+    private let travelPlanDetailsView = TravelPlanDetailsView()
+    
+    // MARK: - life cycles
+    override func loadView() {
+        view = travelPlanDetailsView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
-    */
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.tabBarController?.tabBar.isHidden = false
+    }
+    
+    // MARK: - methods
+    override func configureStyle() {
+        configureNavigation()
+    }
+    
+    override func configureAddTarget() {
+        travelPlanDetailsView.backButton.target = self
+        travelPlanDetailsView.backButton.action = #selector(tappedBackButton)
+        
+        travelPlanDetailsView.settingButton.target = self
+        travelPlanDetailsView.settingButton.action = #selector(tappedSettingButton)
+    }
+    
+    private func configureNavigation() {
+        navigationItem.leftBarButtonItem = travelPlanDetailsView.backButton
+        navigationItem.rightBarButtonItem = travelPlanDetailsView.settingButton
+    }
+    
+    @objc private func tappedBackButton() {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @objc private func tappedSettingButton() {
+        
+    }
 }
