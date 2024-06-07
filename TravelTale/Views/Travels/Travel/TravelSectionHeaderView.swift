8e3df4792d5a4d695fc0eb07c19afba2bc9d9d4b
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-06-08 06:06:42 +0900

    [Add] #19 - (TravelSectionHeaderView) travelPlanView.tableView의SectionHeader뷰

--- TravelTale/Views/Travels/Travel/TravelSectionHeaderView.swift
+++ TravelTale/Views/Travels/Travel/TravelSectionHeaderView.swift
@@ -5,16 +5,41 @@
 //  Created by 유림 on 6/8/24.
 //
 
+import Then
 import UIKit
 
-class TravelSectionHeaderView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class TravelSectionHeaderView: UITableViewHeaderFooterView {
+    
+    // MARK: - properties
+    static let identifier = String(String(describing: TravelSectionHeaderView.self))
+    
+    private let titleLabel = UILabel().then {
+        $0.configureLabel(color: .black, font: .pretendard(size: 18, weight: .bold))
     }
-    */
-
+    
+    
+    // MARK: - methods
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        configureHierarchy()
+        configureConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureHierarchy() {
+        contentView.addSubview(titleLabel)
+    }
+    
+    private func configureConstraints() {
+        titleLabel.snp.makeConstraints {
+            $0.top.leading.equalToSuperview()
+        }
+    }
+    
+    func bind(title: String) {
+        titleLabel.text = title
+    }
 }
