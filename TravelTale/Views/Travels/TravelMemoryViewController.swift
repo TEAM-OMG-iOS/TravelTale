ab246000b0d5cfb2184b12f2371f273ff20f4f4f
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-06-08 01:28:36 +0900

    [Add, Feat] #1 - (TravelMemoryViewController) tableViewDataSource 설정

--- TravelTale/Views/Travels/TravelMemoryViewController.swift
+++ TravelTale/Views/Travels/TravelMemoryViewController.swift
@@ -7,23 +7,79 @@
 
 import UIKit
 
-class TravelMemoryViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class TravelMemoryViewController: BaseViewController {
+    
+    // MARK: - properties
+    private let travelMemoryView = TravelMemoryView()
+    private let travelViewModel = TravelViewModel()
+    
+    // MARK: - life cycles
+    override func loadView() {
+        view = travelMemoryView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addTemporaryData()
     }
-    */
+    
+    // MARK: - methods
+    // 임시 travel 데이터 넣는 함수 (추후 삭제 예정)
+    private func addTemporaryData() {
+        travelViewModel.travelArray.value.append(contentsOf: [
+            Travel(
+                image: nil,
+                title: "24년의 가족 여행",
+                startDate: DateComponents(year: 2024, month: 4, day: 2).date ?? Date(),
+                endDate: DateComponents(year: 2024, month: 4, day: 5).date ?? Date(),
+                province: nil)
+        ])
+    }
+    
+    override func configureStyle() {
+        travelMemoryView.tableView.separatorStyle = .none
+    }
+    
+    override func configureDelegate() {
+        travelMemoryView.tableView.dataSource = self
+        travelMemoryView.tableView.register(TravelTableViewCell.self, forCellReuseIdentifier: TravelTableViewCell.identifier)
+    }
+    
+    override func configureAddTarget() {
+        travelMemoryView.addView.button.addTarget(self, action: #selector(tappedAddButton), for: .touchUpInside)
+    }
+    
+    override func bind() {
+        travelViewModel.travelArray.bind { _ in
+            self.travelMemoryView.tableView.reloadData()
+        }
+    }
+    
+    // MARK: - objc method
+    @objc func tappedAddButton() {
+        print("tappedAddButton")
+    }
+    
+}
 
+// MARK: - Extensions
+extension TravelMemoryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return travelViewModel.travelArray.value.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TravelTableViewCell.identifier, for: indexPath) as? TravelTableViewCell else { return UITableViewCell() }
+        
+        
+        let travel = travelViewModel.travelArray.value[indexPath.row]
+        let travelPeriod = travelViewModel.returnPeriodString(startDate: travel.startDate, endDate: travel.endDate)
+        
+        cell.bind(image: travel.image, title: travel.title, period: travelPeriod, province: travel.province)
+        
+        cell.selectionStyle = .none
+        
+        return cell
+    }
+    
 }
