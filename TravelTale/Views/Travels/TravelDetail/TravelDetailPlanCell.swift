bed66a5413d49e7814d8d3702f22000dba813e8a
KJH <gh7052@gmail.com>
2024-06-08 23:18:01 +0900

    [Add] #8 - (TravelDetailPlanCell) 추가

---
+++ TravelTale/Views/Travels/TravelDetail/TravelDetailPlanCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelDetailPlanCell.swift
+//  TravelTale
+//
+//  Created by 김정호 on 6/8/24.
+//
+
+import UIKit
+
+class TravelDetailPlanCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
