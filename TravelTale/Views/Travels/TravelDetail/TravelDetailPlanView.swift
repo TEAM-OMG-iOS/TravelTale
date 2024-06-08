c57ed35f60a2e225c14cbb138cc7bbdf845fdcaf
KJH <gh7052@gmail.com>
2024-06-08 23:04:48 +0900

    [Add] #8 - (TravelDetailPlanView, TravelDetailPlanViewController) 추가

---
+++ TravelTale/Views/Travels/TravelDetail/TravelDetailPlanView.swift
@@ -0,0 +1,20 @@
+//
+//  TravelDetailPlanView.swift
+//  TravelTale
+//
+//  Created by 김정호 on 6/8/24.
+//
+
+import UIKit
+
+class TravelDetailPlanView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
