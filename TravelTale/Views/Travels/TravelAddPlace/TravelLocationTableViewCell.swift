fd06fb0be9e832738219bfc7fa1d018d7b31186a
yoonhee <parkyunhee1217@gmail.com>
2024-06-10 20:24:42 +0900

    [Add] #5 - (TravelLocationTableViewCell) 추가

---
+++ TravelTale/Views/Travels/TravelAddPlace/TravelLocationTableViewCell.swift
@@ -0,0 +1,66 @@
+//
+//  TravelLocationTableViewCell.swift
+//  TravelTale
+//
+//  Created by SAMSUNG on 6/5/24.
+//
+
+import UIKit
+
+final class TravelLocationTableViewCell: BaseTableViewCell {
+    
+    // MARK: - Properties
+    
+    static let identifier = "LocationTableViewCell"
+    
+    let locationLabel = UILabel().then {
+        $0.configureLabel(alignment: .left, color: .blueBlack100, font: .pretendard(size: 15, weight: .bold))
+    }
+    
+    let locationButton = UIButton()
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        locationLabel.text = ""
+    }
+    
+    // MARK: - Methods
+    
+    override func configureUI() {
+        super.configureUI()
+    }
+    
+    override func configureHierarchy() {
+        [locationLabel, locationButton].forEach {
+            self.addSubview($0)
+        }
+    }
+    
+    override func configureConstraints() { 
+        locationLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(10)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.bottom.equalToSuperview().offset(-10)
+        }
+        
+        locationButton.snp.makeConstraints { 
+            $0.top.equalToSuperview().offset(10)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.bottom.equalToSuperview().offset(-10)
+        }
+    }
+}
+
