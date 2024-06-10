e9ffc3ffcf59defeb5db4d2dfd2b618c1b8900c8
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-06-10 11:55:44 +0900

    [Feat] #21 - (TravelMemoryTravelSelectionView) tableView 생성

--- TravelTale/Views/Travels/Travel/TravelMemoryTravelSelectionView.swift
+++ TravelTale/Views/Travels/Travel/TravelMemoryTravelSelectionView.swift
@@ -10,9 +10,26 @@ import UIKit
 class TravelMemoryTravelSelectionView: BaseView {
     
     // MARK: - properties
-    let titleLabel = UILabel()
-    let exitButtonView = ExitButtonView()
+    let titleLabel = UILabel().then {
+        $0.configureLabel(alignment: .center, color: .black, font: .oaGothic(size: 18, weight: .heavy), text: "추억 남기기")
+    }
+    
+    let exitButtonView = UIButton().then {
+        $0.configureView(color: .gray70)
+        $0.configureButton(fontColor: .white, font: .pretendard(size: 12, weight: .bold), text: "x")
+    }
+    
+    let tableView = UITableView()
     
     // MARK: - methods
+    override func configureHierarchy() {
+        self.addSubview(tableView)
+    }
     
+    override func configureConstraints() {
+        tableView.snp.makeConstraints {
+            $0.verticalEdges.equalTo(self.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+        }
+    }
 }
