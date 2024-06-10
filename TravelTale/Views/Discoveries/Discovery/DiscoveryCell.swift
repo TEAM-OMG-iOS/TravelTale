c6bdf0b5b1c41c5850a66cf373f4f22a50883778
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-06-10 19:29:10 +0900

    [Fix] #6 - (DiscoveryCell) 중복 코드 삭제

--- TravelTale/Views/Discoveries/Discovery/DiscoveryCell.swift
+++ TravelTale/Views/Discoveries/Discovery/DiscoveryCell.swift
@@ -30,14 +30,6 @@ final class DiscoveryCell: BaseCollectionViewCell {
     }
     
     // MARK: - life cycles
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func prepareForReuse() {
         super.prepareForReuse()
         
