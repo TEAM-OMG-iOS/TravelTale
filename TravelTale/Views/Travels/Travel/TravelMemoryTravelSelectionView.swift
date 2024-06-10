8efb894cd2fce16bae2b7190a830177588b09d36
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-06-10 10:36:18 +0900

    [Add] #21 - (TravelMemoryTravelSelectionView) 파일 추가

---
+++ TravelTale/Views/Travels/Travel/TravelMemoryTravelSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  TravelMemorySelectionView.swift
+//  TravelTale
+//
+//  Created by 유림 on 6/10/24.
+//
+
+import UIKit
+
+class TravelMemoryTravelSelectionView: BaseView {
+    
+    // MARK: - properties
+    let titleLabel = UILabel()
+    
+    
+    // MARK: - methods
+    
+}
