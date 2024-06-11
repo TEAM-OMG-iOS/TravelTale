f23e3a77e517850e4f630c645e0f696eb999b012
KJH <gh7052@gmail.com>
2024-06-11 22:44:48 +0900

    [Add] #8 - (TravelDetailPlanHeaderCell) 추가

---
+++ TravelTale/Views/Travels/TravelDetailPlan/TravelDetailPlanHeaderCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelDetailPlanHeaderCell.swift
+//  TravelTale
+//
+//  Created by 김정호 on 6/11/24.
+//
+
+import UIKit
+
+class TravelDetailPlanHeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
