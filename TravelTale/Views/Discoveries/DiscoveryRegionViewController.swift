fe6a2657d1e5cabdca030e15ce9e0071596c6dfe
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-06-11 12:58:59 +0900

    [Feat] #26 - (DiscoveryRegionViewController) discoveryRegionView 연결

--- TravelTale/Views/Discoveries/DiscoveryRegionViewController.swift
+++ TravelTale/Views/Discoveries/DiscoveryRegionViewController.swift
@@ -7,23 +7,17 @@
 
 import UIKit
 
-class DiscoveryRegionViewController: UIViewController {
+final class DiscoveryRegionViewController: BaseViewController {
+    
+    // MARK: - properties
+    private let discoveryRegionView = DiscoveryRegionView()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    // MARK: - life cycles
+    override func loadView() {
+        view = discoveryRegionView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
-    */
-
 }
