0707079b9555fb1a32ab7db46065cfb6ef0ee8ae
KJH <gh7052@gmail.com>
2024-06-12 01:42:16 +0900

    [Add] #8 - (TravelDetailPlanHeaderContentCell) 추가

---
+++ TravelTale/Views/Travels/TravelDetailPlan/TravelDetailPlanHeaderContentCell.swift
@@ -0,0 +1,12 @@
+//
+//  TravelDetailPlanHeaderContentCell.swift
+//  TravelTale
+//
+//  Created by 김정호 on 6/12/24.
+//
+
+import UIKit
+
+class TravelDetailPlanHeaderContentCell: UICollectionViewCell {
+    
+}
