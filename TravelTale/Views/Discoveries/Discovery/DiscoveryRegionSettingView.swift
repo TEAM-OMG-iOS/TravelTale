fdf105160d91fee1cef84ee50ace481adcc8edd3
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-06-11 12:22:02 +0900

    [Design] #26 - (DiscoveryRegionSettingView) 시/도, 구/군 레이블 추가

--- TravelTale/Views/Discoveries/Discovery/DiscoveryRegionSettingView.swift
+++ TravelTale/Views/Discoveries/Discovery/DiscoveryRegionSettingView.swift
@@ -12,12 +12,22 @@ final class DiscoveryRegionSettingView: BaseView {
     // MARK: - properties
     private let cityBackground = GrayBackgroundView()
     
+    private let cityLabel = UILabel().then {
+        $0.configureLabel(font: .pretendard(size: 16, weight: .bold), text: "시/도")
+    }
+    
     private let districtBackground = GrayBackgroundView()
+    
+    private let districtLabel = UILabel().then {
+        $0.configureLabel(font: .pretendard(size: 16, weight: .bold), text: "구/군")
+    }
     
     // MARK: - methods
     override func configureHierarchy() {
         self.addSubview(cityBackground)
+        cityBackground.addSubview(cityLabel)
         self.addSubview(districtBackground)
+        districtLabel.addSubview(districtLabel)
     }
     
     override func configureConstraints() {
@@ -26,9 +36,19 @@ final class DiscoveryRegionSettingView: BaseView {
             $0.top.equalTo(safeAreaLayoutGuide).offset(44)
         }
         
+        cityLabel.snp.makeConstraints {
+            $0.leading.equalTo(cityBackground.snp.leading).offset(16)
+            $0.centerY.equalTo(cityBackground.snp.centerY)
+        }
+        
         districtBackground.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview().inset(24)
             $0.top.equalTo(cityBackground.snp.bottom).offset(24)
+        }
+        
+        districtLabel.snp.makeConstraints {
+            $0.leading.equalTo(districtBackground.snp.leading).offset(16)
+            $0.centerY.equalTo(districtBackground.snp.centerY)
         }
     }
 }
