cb5b1ca528d308899fc98e3b09e139edafa42b13
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-06-11 12:16:32 +0900

    [Design] #26 - (DiscoveryRegionSettingView) 시/도, 구/군 배경뷰 추가

--- TravelTale/Views/Discoveries/Discovery/DiscoveryRegionSettingView.swift
+++ TravelTale/Views/Discoveries/Discovery/DiscoveryRegionSettingView.swift
@@ -5,4 +5,30 @@
 //  Created by 배지해 on 6/11/24.
 //
 
-import Foundation
+import UIKit
+
+final class DiscoveryRegionSettingView: BaseView {
+    
+    // MARK: - properties
+    private let cityBackground = GrayBackgroundView()
+    
+    private let districtBackground = GrayBackgroundView()
+    
+    // MARK: - methods
+    override func configureHierarchy() {
+        self.addSubview(cityBackground)
+        self.addSubview(districtBackground)
+    }
+    
+    override func configureConstraints() {
+        cityBackground.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(24)
+            $0.top.equalTo(safeAreaLayoutGuide).offset(44)
+        }
+        
+        districtBackground.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(24)
+            $0.top.equalTo(cityBackground.snp.bottom).offset(24)
+        }
+    }
+}
