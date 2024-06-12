8fcf2563400a4c47bb92a49c98863fb2111ce304
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-06-12 21:39:18 +0900

    [Design] #30 - (DiscoveryRegionModalView) 테이블 뷰 구현

--- TravelTale/Views/Discoveries/Discovery/DiscoveryRegionModalView.swift
+++ TravelTale/Views/Discoveries/Discovery/DiscoveryRegionModalView.swift
@@ -10,19 +10,28 @@ import UIKit
 final class DiscoveryRegionModalView: BaseView {
     
     // MARK: - properties
-    var selectLabel = UILabel().then {
+    private let selectLabel = UILabel().then {
         $0.configureLabel(font: .pretendard(size: 18, weight: .bold))
     }
+    
+    let regionTableView = UITableView()
     
     // MARK: - methods
     override func configureHierarchy() {
         self.addSubview(selectLabel)
+        self.addSubview(regionTableView)
     }
     
     override func configureConstraints() {
         selectLabel.snp.makeConstraints{
             $0.horizontalEdges.equalToSuperview().inset(24)
             $0.top.equalToSuperview().inset(36)
+        }
+        
+        regionTableView.snp.makeConstraints{
+            $0.horizontalEdges.equalToSuperview().inset(18)
+            $0.top.equalTo(selectLabel.snp.bottom).offset(36)
+            $0.bottom.equalToSuperview()
         }
     }
 }
