39f8f5baaaea4637932103256d5f31bb619c4be1
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-06-12 16:48:48 +0900

    [Design] #21 - (TravelMemoryDetailEditView) 뷰 구성 완료

--- TravelTale/Views/Travels/Travel/TravelMemoryDetailEditView.swift
+++ TravelTale/Views/Travels/Travel/TravelMemoryDetailEditView.swift
@@ -10,34 +10,34 @@ import UIKit
 class TravelMemoryDetailEditView: BaseView {
     
     // MARK: - properties
-    let travelInfoStackView = UIStackView().then {
+    private let travelInfoStackView = UIStackView().then {
         $0.axis = .horizontal
         $0.spacing = 4
     }
     
-    let locationImageView = UIImageView().then {
+    private let locationImageView = UIImageView().then {
         $0.image = UIImage(systemName: "pin")
         $0.tintColor = .gray100
         $0.contentMode = .scaleAspectFit
     }
     
-    let provinceLabel = UILabel().then {
+    private let provinceLabel = UILabel().then {
         $0.configureLabel(color: .gray100, font: .oaGothic(size: 10, weight: .medium))
     }
     
-    let separatorLabel = UILabel().then {
-        $0.configureLabel(color: .gray100, font: .oaGothic(size: 10, weight: .medium))
+    private let separatorLabel = UILabel().then {
+        $0.configureLabel(color: .gray100, font: .oaGothic(size: 10, weight: .medium), text: "|")
     }
     
     private let periodLabel = UILabel().then {
         $0.configureLabel(color: .gray100, font: .oaGothic(size: 10, weight: .medium))
     }
     
-    let travelTitleLabel = UILabel().then {
+    private let travelTitleLabel = UILabel().then {
         $0.configureLabel(alignment: .left, font: .pretendard(size: 20, weight: .bold))
     }
     
-    let borderLine = UIView().then {
+    private let borderLine = UIView().then {
         $0.configureView(color: .gray10)
     }
     
@@ -51,14 +51,56 @@ class TravelMemoryDetailEditView: BaseView {
     
     let recordTextView = UITextView().then {
         $0.configureView(color: .clear)
-        $0.textAlignment = .natural
+        $0.font = .pretendard(size: 16, weight: .regular)
         $0.text = "기록하고 싶은 내용을 작성해주세요"
         $0.textColor = .lightGray
+        
+        $0.textContainerInset = .zero // default: (8, 0, 8, 0)
+        $0.textContainer.lineFragmentPadding = 0 // default : 5.0
     }
     
-    let collectionView = UICollectionView()
+    private let photoButtonView = UIView().then {
+        $0.configureView(color: .white, cornerRadius: 10)
+        $0.layer.shadowRadius = 4
+        $0.layer.shadowOpacity = 0.25
+        $0.layer.shadowOffset = CGSize(width: 2, height: 2)
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.gray5.cgColor
+    }
     
-    let buttonStackView = UIStackView().then {
+    private let photoButtonImageView = UIImageView().then {
+        $0.image = UIImage(systemName: "photo.badge.plus")
+        $0.contentMode = .scaleAspectFit
+        $0.tintColor = .black
+    }
+    
+    private let photoButtonLabelStackView = UIStackView().then {
+        $0.axis = .horizontal
+    }
+    
+    private let photoTotalLabel = UILabel().then {
+        $0.configureLabel(color: .gray80, font: .pretendard(size: 16, weight: .regular), text: "/10")
+    }
+    
+    private let photoCountLabel = UILabel().then {
+        $0.configureLabel(color: .green100, font: .pretendard(size: 16, weight: .medium))
+    }
+    
+    let photoButton = UIButton().then {
+        $0.configureView(color: .clear)
+    }
+    
+    private let layout = UICollectionViewFlowLayout().then {
+        let spacing = 8
+        $0.scrollDirection = .horizontal
+        $0.minimumInteritemSpacing = 8
+        $0.itemSize = .init(width: 82, height: 82)
+    }
+    
+    lazy var collectionView = UICollectionView(frame: .zero,
+                                          collectionViewLayout: layout)
+    
+    private let buttonStackView = UIStackView().then {
         $0.axis = .horizontal
         $0.spacing = 15
     }
@@ -79,6 +121,7 @@ class TravelMemoryDetailEditView: BaseView {
          travelTitleLabel,
          borderLine,
          recordView,
+         photoButtonView,
          collectionView,
          buttonStackView].forEach { self.addSubview($0) }
         
@@ -90,14 +133,19 @@ class TravelMemoryDetailEditView: BaseView {
         [recordTitleLabel,
          recordTextView].forEach { recordView.addSubview($0) }
         
+        [photoButtonImageView,
+         photoButtonLabelStackView].forEach { photoButtonView.addSubview($0) }
+        
+        [photoCountLabel,
+         photoTotalLabel].forEach { photoButtonLabelStackView.addArrangedSubview($0) }
+        
         [formerButton,
          confirmButton].forEach { buttonStackView.addArrangedSubview($0) }
-        
     }
     
     override func configureConstraints() {
         let phoneWidth = UIScreen.main.bounds.width
-        let horizontalInset = 20
+        let horizontalInset = CGFloat(20)
         
         travelInfoStackView.snp.makeConstraints {
             $0.top.equalTo(safeAreaLayoutGuide).offset(10)
@@ -120,7 +168,7 @@ class TravelMemoryDetailEditView: BaseView {
         }
         
         recordView.snp.makeConstraints {
-            $0.top.equalTo(borderLine.snp.bottom).offset(36)
+            $0.top.equalTo(borderLine.snp.bottom).offset(20)
             $0.horizontalEdges.equalToSuperview().inset(horizontalInset)
             $0.height.equalTo(208)
         }
@@ -130,23 +178,49 @@ class TravelMemoryDetailEditView: BaseView {
         }
         
         recordTextView.snp.makeConstraints {
-            $0.top.equalTo(recordTitleLabel).offset(8)
+            $0.top.equalTo(recordTitleLabel.snp.bottom).offset(8)
             $0.bottom.horizontalEdges.equalToSuperview().inset(16)
         }
         
-        collectionView.snp.makeConstraints {
+        photoButtonView.snp.makeConstraints {
             $0.top.equalTo(recordView.snp.bottom).offset(16)
-            $0.horizontalEdges.equalToSuperview().inset(horizontalInset)
-            $0.height.equalTo(82)
+            $0.leading.equalToSuperview().offset(horizontalInset)
+            $0.size.equalTo(80)
+        }
+        
+        photoButtonImageView.snp.makeConstraints {
+            $0.size.equalTo(36)
+            $0.top.equalToSuperview().offset(11.5)
+            $0.centerX.equalToSuperview()
+        }
+        
+        photoButtonLabelStackView.snp.makeConstraints {
+            $0.top.equalTo(photoButtonImageView.snp.bottom).offset(2)
+            $0.centerX.equalToSuperview()
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.bottom.equalTo(photoButtonView)
+            $0.leading.equalTo(photoButtonView.snp.trailing).offset(8)
+            $0.trailing.equalToSuperview().inset(horizontalInset)
         }
         
         buttonStackView.snp.makeConstraints {
             $0.bottom.equalTo(safeAreaLayoutGuide).inset(20)
-            $0.horizontalEdges.equalTo(horizontalInset)
+            $0.horizontalEdges.equalToSuperview().inset(horizontalInset)
         }
         
         formerButton.snp.makeConstraints {
-            $0.width.equalTo(phoneWidth / 3)
+            let buttonsWidth = phoneWidth - horizontalInset * 2 - 15
+            $0.width.equalTo(buttonsWidth / 3)
         }
+    }
+    
+    func bind(travel: Travel) {
+        provinceLabel.text = travel.province ?? "미정"
+        periodLabel.text = String(startDate: travel.startDate, endDate: travel.endDate)
+        travelTitleLabel.text = travel.title
+        
+        photoCountLabel.text = "5"
     }
 }
