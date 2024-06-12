805421cb0562c3f5a88b1a21e6fe277379bb88ee
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-06-12 17:28:08 +0900

    [Feat] #30 - (DiscoveryRegionModalView) 선택 레이블 구현

--- TravelTale/Views/Discoveries/Discovery/DiscoveryRegionModalView.swift
+++ TravelTale/Views/Discoveries/Discovery/DiscoveryRegionModalView.swift
@@ -5,4 +5,24 @@
 //  Created by 배지해 on 6/12/24.
 //
 
-import Foundation
+import UIKit
+
+final class DiscoveryRegionModalView: BaseView {
+    
+    // MARK: - properties
+    var selectLabel = UILabel().then {
+        $0.configureLabel(font: .pretendard(size: 18, weight: .bold))
+    }
+    
+    // MARK: - methods
+    override func configureHierarchy() {
+        self.addSubview(selectLabel)
+    }
+    
+    override func configureConstraints() {
+        selectLabel.snp.makeConstraints{
+            $0.horizontalEdges.equalToSuperview().inset(24)
+            $0.top.equalToSuperview().inset(36)
+        }
+    }
+}
