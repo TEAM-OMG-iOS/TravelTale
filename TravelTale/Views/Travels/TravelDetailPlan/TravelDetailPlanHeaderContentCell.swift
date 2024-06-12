b6964029619a2fee23c416188fc8d3b5affb5929
KJH <gh7052@gmail.com>
2024-06-13 00:48:44 +0900

    [Design] #8 - (TravelDetailPlanHeaderContentCell) UI 구성

--- TravelTale/Views/Travels/TravelDetailPlan/TravelDetailPlanHeaderContentCell.swift
+++ TravelTale/Views/Travels/TravelDetailPlan/TravelDetailPlanHeaderContentCell.swift
@@ -7,6 +7,39 @@
 
 import UIKit
 
-class TravelDetailPlanHeaderContentCell: UICollectionViewCell {
+final class TravelDetailPlanHeaderContentCell: BaseCollectionViewCell {
     
+    // MARK: - properties
+    private let dayLabel = UILabel().then {
+        $0.configureLabel(alignment: .center, color: .green100, font: .oaGothic(size: 15, weight: .heavy))
+    }
+    
+    private let underlineView = UIView().then {
+        $0.configureView(color: .green100)
+    }
+    
+    // MARK: - life cycles
+    override func prepareForReuse() {
+        dayLabel.text = nil
+    }
+    
+    // MARK: - methods
+    override func configureHierarchy() {
+        self.contentView.addSubview(dayLabel)
+        self.contentView.addSubview(underlineView)
+    }
+    
+    override func configureConstraints() {
+        dayLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.centerX.equalToSuperview()
+        }
+        
+        underlineView.snp.makeConstraints {
+            $0.top.equalTo(dayLabel.snp.bottom).offset(4)
+            $0.horizontalEdges.equalTo(dayLabel)
+            $0.bottom.equalToSuperview()
+            $0.height.equalTo(3)
+        }
+    }
 }
