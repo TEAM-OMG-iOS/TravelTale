6b749a23d029364d2dbcc8aefaf853f174957c51
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-06-14 02:24:52 +0900

    [Design] #35 - (bookMarkLabel) 북마크 라벨 구현

--- TravelTale/Views/MyPages/MyPage/MyPageView.swift
+++ TravelTale/Views/MyPages/MyPage/MyPageView.swift
@@ -19,15 +19,24 @@ final class MyPageView: BaseView {
         $0.configureView( color: .green10)
     }
     
+    private let bookMarkLabel = UILabel().then {
+        $0.configureLabel(font: .pretendard(size: 18, weight: .bold), text: "북마크")
+    }
+    
     // MARK: - life cycles
     override func configureHierarchy() {
         self.addSubview(bookMarkBackgroundView)
+        self.addSubview(bookMarkLabel)
     }
     
     override func configureConstraints() {
         bookMarkBackgroundView.snp.makeConstraints {
             $0.verticalEdges.top.equalToSuperview()
             $0.height.equalToSuperview().multipliedBy(0.4143192488)
+        }
+        
+        bookMarkLabel.snp.makeConstraints {
+            $0.top.leading.equalToSuperview().inset(24)
         }
     }
 }
