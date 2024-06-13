44a3b6e70c649e917fef9da7e824f4db314ae0af
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-06-14 00:56:17 +0900

    [Add] #21 - (TravelMemoryDetailViewController) 파일 생성

---
+++ TravelTale/Views/Travels/TravelMemory/TravelMemoryDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TravelMemoryDetailViewController.swift
+//  TravelTale
+//
+//  Created by 유림 on 6/14/24.
+//
+
+import UIKit
+
+class TravelMemoryDetailViewController: BaseViewController {
+    
+    
+}
