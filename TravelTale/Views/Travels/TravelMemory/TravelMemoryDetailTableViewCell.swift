83d0036876b4107f073c4f05cdd7d65413dc80a7
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-06-14 01:21:27 +0900

    [Add] #21 - (TravelMemoryDetailTableViewCell) 파일 생성

---
+++ TravelTale/Views/Travels/TravelMemory/TravelMemoryDetailTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TravelMemoryDetailTableViewCell.swift
+//  TravelTale
+//
+//  Created by 유림 on 6/14/24.
+//
+
+import UIKit
+
+class TravelMemoryDetailTableViewCell: BaseTableViewCell {
+    
+    
+}
