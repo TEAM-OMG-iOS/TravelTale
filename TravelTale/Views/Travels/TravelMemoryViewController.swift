f5894bfd37b87751ac6d3fe1fb93da1b34f4853d
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-06-14 01:54:11 +0900

    [Feat] #21 - (TravelMemoryViewController) 셀 선택 시 memoryDetailVC push, ViewModel 삭제

--- TravelTale/Views/Travels/TravelMemoryViewController.swift
+++ TravelTale/Views/Travels/TravelMemoryViewController.swift
@@ -11,7 +11,11 @@ final class TravelMemoryViewController: BaseViewController {
     
     // MARK: - properties
     private let travelMemoryView = TravelMemoryView()
-    private let travelViewModel = TravelViewModel()
+    private var travels: [Travel] = [] {
+        didSet {
+            travelMemoryView.tableView.reloadData()
+        }
+    }
     
     
     // MARK: - life cycles
@@ -28,7 +32,7 @@ final class TravelMemoryViewController: BaseViewController {
     // MARK: - methods
     // TODO: 임시 travel 데이터 넣는 함수 (추후 삭제 예정)
     private func addTemporaryData() {
-        travelViewModel.travelArray.value.append(contentsOf: [
+        travels.append(contentsOf: [
             Travel(
                 image: nil,
                 title: "24년의 가족 여행",
@@ -36,7 +40,7 @@ final class TravelMemoryViewController: BaseViewController {
                 endDate: createDate(year: 2024, month: 4, day: 5) ?? Date(),
                 province: nil,
                 travelJournal: nil,
-                memoryImages: nil)
+                memoryImages: [])
         ])
     }
     
@@ -56,18 +60,12 @@ final class TravelMemoryViewController: BaseViewController {
     
     override func configureDelegate() {
         travelMemoryView.tableView.dataSource = self
+        travelMemoryView.tableView.delegate = self
         travelMemoryView.tableView.register(TravelTableViewCell.self, forCellReuseIdentifier: TravelTableViewCell.identifier)
     }
     
     override func configureAddTarget() {
         travelMemoryView.addButtonView.button.addTarget(self, action: #selector(tappedAddButton), for: .touchUpInside)
-    }
-    
-    override func bind() { 
-        travelViewModel.travelArray.bind { [weak self] _ in
-            guard let self = self else { return }
-            travelMemoryView.tableView.reloadData()
-        }
     }
     
     
@@ -81,17 +79,28 @@ final class TravelMemoryViewController: BaseViewController {
 // MARK: - Extensions
 extension TravelMemoryViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return travelViewModel.travelArray.value.count
+        return travels.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: TravelTableViewCell.identifier, for: indexPath) as? TravelTableViewCell else { return UITableViewCell() }
         
-        let travel = travelViewModel.travelArray.value[indexPath.row]
-        
-        cell.bind(travel: travel)
+        cell.bind(travel: travels[indexPath.row])
         cell.selectionStyle = .none
         
         return cell
+    }
+}
+
+extension TravelMemoryViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TravelTableViewCell.identifier, for: indexPath) as? TravelTableViewCell else { return }
+        
+        let travelMemoryDetailViewController = TravelMemoryDetailViewController(travelData: travels[indexPath.row])
+        navigationController?.pushViewController(travelMemoryDetailViewController, animated: true)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
     }
 }
