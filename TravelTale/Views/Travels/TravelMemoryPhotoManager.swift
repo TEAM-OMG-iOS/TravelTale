19bd29769de65669827122a39fb82ce4b6b8c916
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-06-13 15:37:36 +0900

    [Add,Feat] #21 - (TravelMemoryPhotoManager) Memory 사진 매니저 설정

--- TravelTale/Views/Travels/TravelMemoryPhotoManager.swift
+++ TravelTale/Views/Travels/TravelMemoryPhotoManager.swift
@@ -11,37 +11,41 @@ class TravelMemoryPhotoManager {
     static let shared = TravelMemoryPhotoManager()
     private init() {}
     
-    // 메모리 절약을 위해 PHPickerResult를 저장
-    // 빠른 이미지 로드를 위해 이미지가 필요한 상황에서만 UIImage를 imageCache에 저장
-    var photoResults: [PHPickerResult] = []
-    private var imageCache: [String:UIImage] = [:]
+    var photoResults: [PHPickerResult] = [] // 메모리 절약을 위해 UIImage 대신 PHPickerResult를 저장
+    private var imageCache: (identifier: String, image: UIImage)?
+    // ㄴ TODO: imageCache를 [String:UIImage]로 변경
+        // 목적: 중복되는 UIImage load를 줄이기 위해
     
     func loadImage(for result: PHPickerResult, completion: @escaping (UIImage?) -> Void) {
-        if let identifier = result.assetIdentifier,
-           let cachedImage = imageCache[identifier] {
-            completion(cachedImage)
+        // TODO: [코드 추가] imageCache 배열에 있는 이미지라면 아래 실행하지 않고 리턴
+        /* 
+         if ~~~~ {
+            completion(image)
             return
         }
-        
+         */
+       
         let itemProvider = result.itemProvider
         if itemProvider.canLoadObject(ofClass: UIImage.self) {
             itemProvider.loadObject(ofClass: UIImage.self) { [weak self] object, error in
                 guard let self = self else { return }
                 if let image = object as? UIImage {
                     if let identifier = result.assetIdentifier {
-                        self.imageCache[identifier] = image
+                        self.imageCache = (identifier, image)
                     }
                     completion(image)
                 } else {
-                    print("[Fail] itemProvider -> UIImage")
+                    print("[Fail]: itemProvider -> UIImage")
                     completion(nil)
                 }
             }
         } else {
-            print("[Fail] itemProvider.canLoadObject")
+            print("[Fail]: itemProvider.canLoadObject")
             completion(nil)
         }
-        
     }
     
+    func clearPhotoResults() {
+        photoResults.removeAll()
+    }
 }
