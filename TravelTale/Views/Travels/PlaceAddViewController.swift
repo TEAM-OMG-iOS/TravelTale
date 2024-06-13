e28a88c9b4862d847d3895f2d90d40fe88d75d3b
Bread-kn72 <ordinarytom72@gmail.com>
2024-06-13 14:10:03 +0900

    [Feat] #14 (PlaceAddEditView, PlaceAddViewController, PlaceEditionViewController) 장소 추가, 수정 페이지 추가

--- TravelTale/Views/Travels/PlaceAddViewController.swift
+++ TravelTale/Views/Travels/PlaceAddViewController.swift
@@ -7,23 +7,151 @@
 
 import UIKit
 
-class PlaceAdditionViewController: UIViewController {
-
+class PlaceAdditionViewController: BaseViewController {
+    
+    // MARK: - IBOutlet
+    @IBOutlet weak var placeContents: UILabel!
+    @IBOutlet weak var startTimeContents: UILabel!
+    @IBOutlet weak var startTimeBtn: UIButton!
+    @IBOutlet weak var memoTV: UITextView!
+    @IBOutlet weak var scheduleContents: UILabel!
+    @IBOutlet weak var naviTitle: UINavigationItem!
+    @IBOutlet weak var completedBtn: UIButton!
+    
+    // MARK: - properties
+    private let timePopoverVC = TimeSelectPopoverViewController()
+    
+    var selectedTime: Date?
+    
+    // MARK: - life cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(updateSelectedTime), name: .selectedTimeUpdated, object: nil)
+        checkBlackText()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - methods
+    override func configureDelegate() {
+        memoTV.delegate = self
     }
-    */
+    
+    override func bind() {
+        // TODO: - 일정 데이터 추가 시 수정 예정
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    private func setBeginText(textView: UITextView) {
+        if textView.text == "메모를 입력해주세요" {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    private func setEndText(textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "메모를 입력해주세요"
+            textView.textColor = .gray80
+        }
+    }
+    
+    private func configurePopover(for popoverVC: UIViewController, sourceButton: UIButton) {
+        popoverVC.modalPresentationStyle = .popover
+        popoverVC.preferredContentSize = CGSize(width: 300, height: 200)
+        let popoverPresentationController = popoverVC.popoverPresentationController
+        popoverPresentationController?.sourceView = sourceButton
+        popoverPresentationController?.sourceRect = CGRect(
+            origin: CGPoint(x: sourceButton.bounds.maxX - 50, y: sourceButton.bounds.midY + 10),
+            size: .zero
+        )
+        popoverPresentationController?.permittedArrowDirections = .up
+    }
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return UIModalPresentationStyle.none
+    }
+    
+    private func checkBlackText() {
+        var scheduleIsBlack = false
+        var startTimeIsBlack = false
+        
+        if placeContents.text != "어디로" {
+            placeContents.textColor = .black
+            scheduleIsBlack = true
+        } else {
+            placeContents.textColor = .gray80
+        }
+        
+        if startTimeContents.text != "시간을 선택하세요" {
+            startTimeContents.textColor = .black
+            startTimeIsBlack = true
+        } else {
+            startTimeContents.textColor = .gray80
+        }
+        
+        if scheduleIsBlack && startTimeIsBlack {
+            completedBtn.isEnabled = true
+            completedBtn.backgroundColor = .green100
+        } else {
+            completedBtn.isEnabled = false
+            completedBtn.backgroundColor = .green10
+        }
+    }
+    
+    private func dateFormat(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "a hh:mm"
+        return formatter.string(from: date)
+    }
+    
+    private func bindingTime() {
+        startTimeContents.text = dateFormat(date: selectedTime ?? Date())
+    }
+    
+    // MARK: - objc func
+    @objc func updateSelectedTime(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let selectedTime = userInfo["selectedTime"] as? Date else { return }
+        
+        self.selectedTime = selectedTime
+        self.bindingTime()
+        self.checkBlackText()
+    }
+    
+    // MARK: - IBAction
+    @IBAction func tappedPlaceBtn(_ sender: UIButton) {
+        // TODO: - 장소 검색 화면으로 이동 구현 예정
+        print("move")
+    }
+    
+    @IBAction func tappedCompletedBtn(_ sender: UIButton) {
+        print("close")
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func tappedExitBtn(_ sender: UIButton) {
+        print("close")
+        navigationController?.popViewController(animated: true)
+    }
+}
 
+extension PlaceAdditionViewController: UIPopoverPresentationControllerDelegate {
+    @IBAction func tappedStartTimeBtn(_ sender: UIButton) {
+        configurePopover(for: timePopoverVC, sourceButton: startTimeBtn)
+        timePopoverVC.popoverPresentationController?.delegate = self
+        present(timePopoverVC, animated: true)
+    }
+}
+
+extension PlaceAdditionViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        setBeginText(textView: textView)
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        setEndText(textView: textView)
+    }
 }
--- TravelTale/Views/Travels/PlaceEditionViewController.swift
+++ TravelTale/Views/Travels/PlaceEditionViewController.swift
@@ -5,4 +5,160 @@
 //  Created by Kinam on 6/13/24.
 //
 
-import Foundation
+import UIKit
+
+class PlaceEditionViewController: BaseViewController {
+    
+    // MARK: - IBOutlet
+    @IBOutlet weak var placeContents: UILabel!
+    @IBOutlet weak var startTimeContents: UILabel!
+    @IBOutlet weak var startTimeBtn: UIButton!
+    @IBOutlet weak var memoTV: UITextView!
+    @IBOutlet weak var scheduleContents: UILabel!
+    @IBOutlet weak var scheduleBtn: UIButton!
+    @IBOutlet weak var naviTitle: UINavigationItem!
+    @IBOutlet weak var completedBtn: UIButton!
+    
+    // MARK: - properties
+    private let dayPopoverVC = DaySelectPopoverViewController()
+    
+    private let timePopoverVC = TimeSelectPopoverViewController()
+    
+    var selectedDays: String?
+    
+    var selectedTime: Date?
+    
+    // MARK: - life cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(updateSelectedDays), name: .selectedDaysUpdated, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(updateSelectedTime), name: .selectedTimeUpdated, object: nil)
+    }
+    
+    // MARK: - methods
+    override func configureDelegate() {
+        memoTV.delegate = self
+    }
+    
+    override func bind() {
+        // TODO: - 일정 데이터 추가 시 수정 예정
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    private func setBeginText(textView: UITextView) {
+        if textView.text == "메모를 입력해주세요" {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    private func setEndText(textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "메모를 입력해주세요"
+            textView.textColor = .gray80
+        }
+    }
+    
+    private func configurePopover(for popoverVC: UIViewController, sourceButton: UIButton) {
+        popoverVC.modalPresentationStyle = .popover
+        popoverVC.preferredContentSize = CGSize(width: 300, height: 200)
+        let popoverPresentationController = popoverVC.popoverPresentationController
+        popoverPresentationController?.sourceView = sourceButton
+        popoverPresentationController?.sourceRect = CGRect(
+            origin: CGPoint(x: sourceButton.bounds.maxX - 50, y: sourceButton.bounds.midY + 10),
+            size: .zero
+        )
+        popoverPresentationController?.permittedArrowDirections = .up
+    }
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return UIModalPresentationStyle.none
+    }
+    
+    private func dateFormat(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "a hh:mm"
+        return formatter.string(from: date)
+    }
+    
+    private func bindingDays() {
+        scheduleContents.text = selectedDays
+    }
+    
+    private func bindingTime() {
+        startTimeContents.text = dateFormat(date: selectedTime ?? Date())
+    }
+    
+    // MARK: - objc func
+    @objc func updateSelectedDays(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let selectedDays = userInfo["selectedDays"] as? String else { return }
+        
+        self.selectedDays = selectedDays
+        self.bindingDays()
+    }
+    
+    @objc func updateSelectedTime(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let selectedTime = userInfo["selectedTime"] as? Date else { return }
+        
+        self.selectedTime = selectedTime
+        self.bindingTime()
+    }
+    
+    // MARK: - IBAction
+    @IBAction func tappedPlaceBtn(_ sender: UIButton) {
+        // TODO: - 장소 검색 화면으로 이동 구현 예정
+        print("move")
+    }
+    
+    @IBAction func tappedCompletedBtn(_ sender: UIButton) {
+        print("close")
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func tappedExitBtn(_ sender: UIButton) {
+        print("close")
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func tappedDeleteBtn(_ sender: UIButton) {
+        // TODO: - 데이터 삭제 로직 구현 예정
+        print("delete")
+        navigationController?.popViewController(animated: true)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+}
+
+extension PlaceEditionViewController: UIPopoverPresentationControllerDelegate {
+    @IBAction func tappedScheduleBtn(_ sender: UIButton) {
+        configurePopover(for: dayPopoverVC, sourceButton: scheduleBtn)
+        dayPopoverVC.popoverPresentationController?.delegate = self
+        present(dayPopoverVC, animated: true)
+    }
+    
+    @IBAction func tappedStartTimeBtn(_ sender: UIButton) {
+        configurePopover(for: timePopoverVC, sourceButton: startTimeBtn)
+        timePopoverVC.popoverPresentationController?.delegate = self
+        present(timePopoverVC, animated: true)
+    }
+}
+
+extension PlaceEditionViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        setBeginText(textView: textView)
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        setEndText(textView: textView)
+    }
+}
