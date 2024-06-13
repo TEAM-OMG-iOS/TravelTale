7b929518104eff9f6cb3909c3289a87566f4d419
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-06-14 02:17:56 +0900

    [Design] #35 - (MyPageView) 북마크 배경 구현

--- TravelTale/Views/MyPages/MyPage/MyPageView.swift
+++ TravelTale/Views/MyPages/MyPage/MyPageView.swift
@@ -10,9 +10,24 @@ import UIKit
 final class MyPageView: BaseView {
     
     // MARK: - properties
-    
     let mypageLabel = UIBarItem().then {
         $0.setTitleTextAttributes([ .font: UIFont.oaGothic(size: 20, weight: .heavy) ], for: .normal)
         $0.title = "마이페이지"
+    }
+    
+    private let bookMarkBackgroundView = UIView().then {
+        $0.configureView( color: .green10)
+    }
+    
+    // MARK: - life cycles
+    override func configureHierarchy() {
+        self.addSubview(bookMarkBackgroundView)
+    }
+    
+    override func configureConstraints() {
+        bookMarkBackgroundView.snp.makeConstraints {
+            $0.verticalEdges.top.equalToSuperview()
+            $0.height.equalToSuperview().multipliedBy(0.4143192488)
+        }
     }
 }
