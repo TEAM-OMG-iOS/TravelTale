9db02a4ec1c15cf8b7a3b3fca813add65732ef08
yoonhee <parkyunhee1217@gmail.com>
2024-06-14 17:36:01 +0900

    [Feat] #23 - (TravelRescheduleViewController) 모달 창으로 이동 구현

--- TravelTale/Views/Travels/TravelReschedule/TravelRescheduleViewController.swift
+++ TravelTale/Views/Travels/TravelReschedule/TravelRescheduleViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 final class TravelRescheduleViewController: BaseViewController {
     
     let travelRescheduleView = TravelRescheduleView()
+    let addLocationView = LocationView()
+    let travelAddCalenderView = TravelAddCalenderView()
     
     override func loadView() {
         view = travelRescheduleView
@@ -24,7 +26,59 @@ final class TravelRescheduleViewController: BaseViewController {
     
     override func configureDelegate() { }
     
-    override func configureAddTarget() { }
+    override func configureAddTarget() {
+        travelRescheduleView.textField.addTarget(self, action: #selector(changedButtonColor), for: .editingChanged)
+        travelRescheduleView.okButton.addTarget(self, action: #selector(tappedSetAlert), for: .touchUpInside)
+        travelRescheduleView.placePickImageButton.addTarget(self, action: #selector(tappedPlacePickButton), for: .touchUpInside)
+        travelRescheduleView.datePickButton.addTarget(self, action: #selector(tappedDatePickButton), for: .touchUpInside)
+    }
     
     override func bind() { }
+    
+    func updateInputBox(with text: String) {
+        travelRescheduleView.placePickImageButton.setTitle(text, for: .normal)
+    }
+    
+    @objc func tappedPlacePickButton() {
+        let locationList = TravelAddLocationViewController()
+        if let sheet = locationList.sheetPresentationController {
+            sheet.detents = [.medium()]
+            sheet.prefersGrabberVisible = true
+            locationList.completion = { [weak self] text in
+                guard let self = self else { return }
+                
+                travelRescheduleView.placePickLabel.text = text
+                travelRescheduleView.placePickLabel.textColor = .black
+            }
+            
+            self.present(locationList, animated: true, completion: nil)
+        }
+    }
+    
+    @objc func tappedDatePickButton() {
+        let calendar = TravelRenewCalendarViewController(monthsLayout: .vertical)
+        if let sheet = calendar.sheetPresentationController {
+            sheet.detents = [.medium()]
+            sheet.prefersGrabberVisible = true
+            calendar.completion = { [weak self] text in
+                guard let self = self else { return }
+                
+                travelRescheduleView.dateLabel.text = text
+            }
+            
+            self.present(calendar, animated: true, completion: nil)
+        }
+    }
+    
+    @objc func changedButtonColor() {
+        travelRescheduleView.buttonColorChanged()
+    }
+    
+    @objc func tappedSetAlert() {
+        self.present(travelRescheduleView.alert, animated: true, completion: nil)
+    }
+    
+    @objc func tappedToRootView() {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
 }
