aacacfc982dca410a2c37801681cce56161a3fc0
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-06-14 23:25:51 +0900

    [Feat] #35 - (MyPageViewController) 마이 페이지 버튼 addTarget 함수 구현

--- TravelTale/Views/MyPages/MyPageViewController.swift
+++ TravelTale/Views/MyPages/MyPageViewController.swift
@@ -12,6 +12,16 @@ class MyPageViewController: BaseViewController {
     // MARK: - properties
     private let myPageView = MyPageView()
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.tabBarController?.tabBar.isHidden = false
+    }
+    
     // MARK: - life cycles
     override func loadView() {
         view = myPageView
@@ -26,7 +36,24 @@ class MyPageViewController: BaseViewController {
         configureNavigationBar()
     }
     
+    override func configureAddTarget() {
+        myPageView.totalButton.addTarget(self, action: #selector(tappedTotalButton), for: .touchUpInside)
+        
+        [myPageView.toristSpotBookMarkButton,
+         myPageView.accommodationBookMarkButton,
+         myPageView.entertainmentBookMarkButton,
+         myPageView.restaurantBookMarkButton].forEach { $0.addTarget(self, action: #selector(tappedCategoryButton), for: .touchUpInside) }
+    }
+    
     private func configureNavigationBar() {
         navigationItem.leftBarButtonItem = myPageView.myPageBarItem
+    }
+    
+    @objc private func tappedTotalButton() {
+        // TODO : 카테고리 버튼 Navi로 띄우기
+    }
+    
+    @objc private func tappedCategoryButton() {
+        // TODO : 카테고리 버튼 Navi로 띄우기
     }
 }
