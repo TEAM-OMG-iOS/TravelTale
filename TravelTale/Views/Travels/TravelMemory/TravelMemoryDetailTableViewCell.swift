b71a6ad59c810a858b099621e0b0959d2eec5ca4
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-06-14 16:49:50 +0900

    [Design] #21 - tableView horizontalEdges 제거, cell에 horizontalEdges 추가

--- TravelTale/Views/Travels/TravelMemory/TravelMemoryDetailTableViewCell.swift
+++ TravelTale/Views/Travels/TravelMemory/TravelMemoryDetailTableViewCell.swift
@@ -9,5 +9,25 @@ import UIKit
 
 class TravelMemoryDetailTableViewCell: BaseTableViewCell {
     
+    static let identifier = String(describing: TravelMemoryDetailTableViewCell.self)
     
+    private let customImageView = UIImageView().then {
+        $0.configureView(clipsToBounds: true, cornerRadius: 10)
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    override func configureHierarchy() { 
+        contentView.addSubview(customImageView)
+    }
+    
+    override func configureConstraints() { 
+        customImageView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(24)
+            $0.verticalEdges.equalToSuperview().inset(8)
+        }
+    }
+    
+    func bind(image: UIImage) {
+        customImageView.image = image
+    }
 }
