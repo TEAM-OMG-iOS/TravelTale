78e06f125537fb0681e73f14e49a7cef570d181d
yoonhee <parkyunhee1217@gmail.com>
2024-06-15 13:04:42 +0900

    [Fix] (SceneDelegate) 원상복구

--- TravelTale/SceneDelegate.swift
+++ TravelTale/SceneDelegate.swift
@@ -17,14 +17,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-             
-             window = UIWindow(windowScene: windowScene)
-             let mainViewController = TravelAddTitleViewController()
-             let navigationController = UINavigationController(rootViewController: mainViewController)
-             window?.rootViewController = navigationController
-             window?.makeKeyAndVisible()
-         }
-    
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = TabBarViewController()
+        window?.makeKeyAndVisible()
+    }
+           
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
