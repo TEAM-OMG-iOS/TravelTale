5a1d0aad4d47106a1deb554546da57ea22fbeafc
yoonhee <parkyunhee1217@gmail.com>
2024-06-15 22:47:43 +0900

    [Fix] #23 - (TravelRenewCalendarView, TravelRenewCalendarViewController) 캘린더 관련 코드 이동 및 정리

--- TravelTale/Views/Travels/TravelReschedule/TravelRenewCalendarView.swift
+++ TravelTale/Views/Travels/TravelReschedule/TravelRenewCalendarView.swift
@@ -8,9 +8,32 @@
 import UIKit
 import HorizonCalendar
 
-final class TravelRenewCalendarView: BaseView {
+final class TravelRenewCalendarView: BaseView, CalendarBaseView {
     
     // MARK: - properties
+    lazy var calendarView = CalendarView(initialContent: makeContent())
+    
+    lazy var calendar = Calendar.current
+    let monthsLayout: MonthsLayout
+    
+    private var selectedDate: Date?
+    var selectedDayRange: DayComponentsRange?
+    private var selectedDayRangeAtStartOfDrag: DayComponentsRange?
+    
+    var completion: ((String) -> Void)?
+    var dateCompletion: ((String) -> Void)?
+    
+    lazy var dayDateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.calendar = calendar
+        dateFormatter.locale = calendar.locale
+        dateFormatter.dateFormat = DateFormatter.dateFormat(
+            fromTemplate: "EEEE, MMM d, yyyy",
+            options: 0,
+            locale: calendar.locale ?? Locale.current)
+        return dateFormatter
+    }()
+    
     let inputTitleLabel = UILabel().then {
         $0.configureLabel(color: .blueBlack100,
                           font: .pretendard(size: 18, weight: .semibold),
@@ -25,11 +48,26 @@ final class TravelRenewCalendarView: BaseView {
                          cornerRadius: 24)
     }
     
+    // MARK: - life cycles
+    required init(monthsLayout: MonthsLayout) {
+        self.monthsLayout = monthsLayout
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     // MARK: - methods
+    override func configureUI() {
+        super.configureUI()
+        selectionHandler()
+    }
+    
     override func configureHierarchy() {
-        
         [inputTitleLabel,
-         okButton].forEach {
+         okButton,
+         calendarView].forEach {
             self.addSubview($0)
         }
     }
@@ -47,5 +85,70 @@ final class TravelRenewCalendarView: BaseView {
             $0.horizontalEdges.equalToSuperview().inset(20)
             $0.height.equalTo(52)
         }
+        
+        calendarView.snp.makeConstraints {
+            $0.top.equalTo(inputTitleLabel.snp.bottom).offset(28)
+            $0.horizontalEdges.equalToSuperview().inset(30)
+            $0.bottom.equalTo(okButton.snp.top).offset(-12)
+        }
+    }
+    
+    private func selectionHandler() {
+        calendarView.daySelectionHandler = { [weak self] day in
+            guard let self else { return }
+            
+            DayRangeSelectionHelper.updateDayRange(
+                afterTapSelectionOf: day,
+                existingDayRange: &selectedDayRange)
+            
+            selectedDate = calendar.date(from: day.components)
+            self.setDateLabel()
+            calendarView.setContent(makeContent())
+        }
+        
+        calendarView.multiDaySelectionDragHandler = { [weak self, calendar] day, state in
+            guard let self else { return }
+            
+            DayRangeSelectionHelper.updateDayRange(
+                afterDragSelectionOf: day,
+                existingDayRange: &selectedDayRange,
+                initialDayRange: &selectedDayRangeAtStartOfDrag,
+                state: state,
+                calendar: calendar)
+            
+            self.setDateLabel()
+            calendarView.setContent(makeContent())
+        }
+    }
+    
+    private func setDateLabel() {
+        if let startDate = calendar.date(from: selectedDayRange?.lowerBound.components ?? DateComponents()),
+           let endDate = calendar.date(from: selectedDayRange?.upperBound.components ?? DateComponents()) {
+            setStartDate(startDate: startDate, endDate: endDate)
+            
+            if startDate == endDate {
+                okButton.isEnabled = true
+                okButton.backgroundColor = .green100
+                okButton.setTitle("당일치기", for: .normal)
+                completion?("당일치기")
+                
+            } else {
+                if let days = calendar.dateComponents([.day], from: startDate, to: endDate).day {
+                    let n = days + 1
+                    okButton.setTitle("\(n-1)박 \(n)일", for: .normal)
+                    
+                    completion?("\(n-1)박 \(n)일")
+                }
+            }
+        }
+    }
+    
+    private func setStartDate(startDate: Date, endDate: Date) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy.MM.dd"
+        let startDateString = dateFormatter.string(from: startDate)
+        let endDateString = dateFormatter.string(from: endDate)
+        let dateRangeString = "\(startDateString) - \(endDateString)"
+        dateCompletion?(dateRangeString)
     }
 }
--- TravelTale/Views/Travels/TravelReschedule/TravelRenewCalendarViewController.swift
+++ TravelTale/Views/Travels/TravelReschedule/TravelRenewCalendarViewController.swift
@@ -8,32 +8,10 @@
 import UIKit
 import HorizonCalendar
 
-final class TravelRenewCalendarViewController: BaseViewController, CalendarViewController {
+final class TravelRenewCalendarViewController: BaseViewController {
     
     // MARK: - properties
-    private let travelRenewCalendarView = TravelRenewCalendarView()
-    lazy var calendarView = CalendarView(initialContent: makeContent())
-    
-    lazy var calendar = Calendar.current
-    let monthsLayout: MonthsLayout
-    
-    private var selectedDate: Date?
-    var selectedDayRange: DayComponentsRange?
-    private var selectedDayRangeAtStartOfDrag: DayComponentsRange?
-    
-    var completion: ((String) -> Void)?
-    var dateCompletion: ((String) -> Void)?
-    
-    lazy var dayDateFormatter: DateFormatter = {
-        let dateFormatter = DateFormatter()
-        dateFormatter.calendar = calendar
-        dateFormatter.locale = calendar.locale
-        dateFormatter.dateFormat = DateFormatter.dateFormat(
-            fromTemplate: "EEEE, MMM d, yyyy",
-            options: 0,
-            locale: calendar.locale ?? Locale.current)
-        return dateFormatter
-    }()
+    let travelRenewCalendarView = TravelRenewCalendarView(monthsLayout: .vertical)
     
     // MARK: - lifecycle
     override func loadView() {
@@ -46,99 +24,16 @@ final class TravelRenewCalendarViewController: BaseViewController, CalendarViewC
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        selectionHandler()
-        configureConstraints()
         
         travelRenewCalendarView.okButton.isEnabled = false
         travelRenewCalendarView.okButton.setTitle("2박 3일", for: .normal)
-    }
-    
-    required init(monthsLayout: MonthsLayout) {
-        self.monthsLayout = monthsLayout
-        super.init(nibName: nil, bundle: nil)
-        
-        selectedDate = calendar.date(from: DateComponents(year: 2020, month: 01, day: 19))!
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
     // MARK: - methods
     override func configureAddTarget() {
         travelRenewCalendarView.okButton.addTarget(self, action: #selector(tappedSetAlert), for: .touchUpInside)
     }
-    
-    private func configureConstraints() {
-        view.addSubview(calendarView)
-        
-        calendarView.snp.makeConstraints {
-            $0.top.equalTo(travelRenewCalendarView.inputTitleLabel.snp.bottom).offset(28)
-            $0.horizontalEdges.equalToSuperview().inset(30)
-            $0.bottom.equalTo(travelRenewCalendarView.okButton.snp.top).offset(-12)
-        }
-    }
-    
-    private func selectionHandler() {
-        calendarView.daySelectionHandler = { [weak self] day in
-            guard let self else { return }
-            
-            DayRangeSelectionHelper.updateDayRange(
-                afterTapSelectionOf: day,
-                existingDayRange: &selectedDayRange)
-            
-            selectedDate = calendar.date(from: day.components)
-            self.setDateLabel()
-            calendarView.setContent(makeContent())
-        }
-        
-        calendarView.multiDaySelectionDragHandler = { [weak self, calendar] day, state in
-            guard let self else { return }
-            
-            DayRangeSelectionHelper.updateDayRange(
-                afterDragSelectionOf: day,
-                existingDayRange: &selectedDayRange,
-                initialDayRange: &selectedDayRangeAtStartOfDrag,
-                state: state,
-                calendar: calendar)
-            
-            self.setDateLabel()
-            calendarView.setContent(makeContent())
-        }
-    }
-    
-    private func setDateLabel() {
-        if let startDate = calendar.date(from: selectedDayRange?.lowerBound.components ?? DateComponents()),
-           let endDate = calendar.date(from: selectedDayRange?.upperBound.components ?? DateComponents()) {
-            setStartDate(startDate: startDate, endDate: endDate)
-            
-            if startDate == endDate {
-                travelRenewCalendarView.okButton.isEnabled = true
-                travelRenewCalendarView.okButton.backgroundColor = .green100
-                travelRenewCalendarView.okButton.setTitle("당일치기", for: .normal)
-                completion?("당일치기")
-                
-            } else {
-                if let days = calendar.dateComponents([.day], from: startDate, to: endDate).day {
-                    let n = days + 1
-                    travelRenewCalendarView.okButton.setTitle("\(n-1)박 \(n)일", for: .normal)
-                    
-                    completion?("\(n-1)박 \(n)일")
-                }
-            }
-        }
-    }
-    
-    private func setStartDate(startDate: Date, endDate: Date) {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy.MM.dd"
-        let startDateString = dateFormatter.string(from: startDate)
-        let endDateString = dateFormatter.string(from: endDate)
-        
-        let dateRangeString = "\(startDateString) - \(endDateString)"
-        dateCompletion?(dateRangeString)
-    }
-    
+
     private func setAlert() {
         let alert = UIAlertController(title: "경고", message: """
     수정된 일정만큼 일부 삭제될 수 있습니다.
