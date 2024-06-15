c5c1da038f4a6ab758174eace4399e814a969049
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-06-16 01:03:33 +0900

    [Design] #40 - (DiscoveryCategoryTableViewCell) cell 화면 구현

--- TravelTale/Views/Discoveries/DiscoveryCategory/DiscoveryCategoryTableViewCell.swift
+++ TravelTale/Views/Discoveries/DiscoveryCategory/DiscoveryCategoryTableViewCell.swift
@@ -5,8 +5,56 @@
 //  Created by 배지해 on 6/16/24.
 //
 
-import SnapKit
+import UIKit
 
 final class DiscoveryCategoryTableViewCell: BaseTableViewCell {
     
+    // MARK: - properties
+    private let placeImage = UIImageView().then {
+        $0.configureView(clipsToBounds: true, cornerRadius: 15)
+    }
+    
+    private let placeLabel = UILabel().then {
+        $0.configureLabel(font: .pretendard(size: 16, weight: .medium))
+    }
+    
+    private let placeAddressLabel = UILabel().then {
+        $0.configureLabel(font: .pretendard(size: 10, weight: .regular))
+    }
+    
+    private let bookmarkButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "bookmark"), for: .normal)
+        $0.tintColor = .green100
+    }
+    
+    // MARK: - methods
+    override func configureHierarchy() {
+        addSubview(placeImage)
+        addSubview(placeLabel)
+        addSubview(placeAddressLabel)
+        addSubview(bookmarkButton)
+    }
+    
+    override func configureConstraints() {
+        placeImage.snp.makeConstraints {
+            $0.horizontalEdges.top.equalToSuperview().inset(16)
+        }
+        
+        placeLabel.snp.makeConstraints {
+            $0.top.equalTo(placeImage.snp.bottom).offset(12)
+            $0.leading.equalToSuperview().inset(16)
+        }
+        
+        placeAddressLabel.snp.makeConstraints {
+            $0.top.equalTo(placeLabel.snp.bottom).offset(8)
+            $0.leading.bottom.equalToSuperview().inset(16)
+            $0.trailing.equalTo(placeLabel)
+        }
+        
+        bookmarkButton.snp.makeConstraints {
+            $0.leading.equalTo(placeLabel.snp.trailing).offset(8)
+            $0.trailing.equalTo(placeImage)
+            $0.size.equalTo(24)
+        }
+    }
 }
