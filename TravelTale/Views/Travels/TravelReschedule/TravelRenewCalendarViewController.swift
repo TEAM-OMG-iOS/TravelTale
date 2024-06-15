a4f5981f12278db16174b79b706a561a8553f95d
yoonhee <parkyunhee1217@gmail.com>
2024-06-15 12:57:02 +0900

    [Feat] #23 - (TravelRenewCalendarViewController) alert 기능 추가

--- TravelTale/Views/Travels/TravelReschedule/TravelRenewCalendarViewController.swift
+++ TravelTale/Views/Travels/TravelReschedule/TravelRenewCalendarViewController.swift
@@ -139,4 +139,31 @@ final class TravelRenewCalendarViewController: BaseViewController, CalendarViewC
         let dateRangeString = "\(startDateString) - \(endDateString)"
         dateCompletion?(dateRangeString)
     }
+    
+    private func setAlert() {
+        let alert = UIAlertController(title: "경고", message: """
+    수정된 일정만큼 일부 삭제될 수 있습니다.
+    그대로 진행하시겠습니까?
+    """, preferredStyle: UIAlertController.Style.alert)
+        
+        let cancel = UIAlertAction(title: "취소", style: .destructive) { [weak self] _ in
+            guard let self = self else { return }
+            //TODO: 기존 날짜로 초기화되는 기능 추가
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        let ok = UIAlertAction(title: "확인", style: .default) { [weak self] _ in
+            guard let self = self else { return }
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        alert.addAction(cancel)
+        alert.addAction(ok)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @objc func tappedSetAlert() {
+        setAlert()
+    }
 }
