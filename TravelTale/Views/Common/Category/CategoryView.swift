277f20ac4305791aa1cc453995ac99110a92176f
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-06-16 01:11:40 +0900

    [Feat] #40 - (CategoryView) 테이블 뷰 그리기

--- TravelTale/Views/Common/Category/CategoryView.swift
+++ TravelTale/Views/Common/Category/CategoryView.swift
@@ -5,3 +5,26 @@
 //  Created by 배지해 on 6/16/24.
 //
 
+import UIKit
+
+final class CategoryView: BaseView {
+    
+    // MARK: - properties
+    let tableView = UITableView()
+    
+    // MARK: - methods
+    override func configureUI() {
+        super.configureUI()
+    }
+    
+    override func configureHierarchy() {
+        addSubview(tableView)
+    }
+    
+    override func configureConstraints() {
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide)
+            $0.horizontalEdges.bottom.equalToSuperview()
+        }
+    }
+}
