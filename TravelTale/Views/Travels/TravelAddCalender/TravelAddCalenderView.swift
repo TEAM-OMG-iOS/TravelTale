93d4e05058f41d6adcba302d97cbc9a1a9743626
yoonhee <parkyunhee1217@gmail.com>
2024-06-16 20:46:20 +0900

    [Fix] #22 - (TravelAddCalenderView, TravelAddCalenderViewController, TravelCalendarItem) 띄어쓰기 수정

--- TravelTale/Views/Travels/TravelAddCalender/TravelAddCalenderView.swift
+++ TravelTale/Views/Travels/TravelAddCalender/TravelAddCalenderView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+
 import HorizonCalendar
 
 final class TravelAddCalenderView: BaseView, CalendarBaseView {
--- TravelTale/Views/Travels/TravelAddCalender/TravelAddCalenderViewController.swift
+++ TravelTale/Views/Travels/TravelAddCalender/TravelAddCalenderViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+
 import HorizonCalendar
 
 class TravelAddCalenderViewController: BaseViewController {
