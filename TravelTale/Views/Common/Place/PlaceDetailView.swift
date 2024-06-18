0daf00edca79a94a3f70e4dd9568f6a5666f31da
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-06-18 14:22:06 +0900

    [Add] #48 - (PlaceDetailView, PlaceDetailViewController) 추가

---
+++ TravelTale/Views/Common/Place/PlaceDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  PlaceDetailView.swift
+//  TravelTale
+//
+//  Created by 배지해 on 6/18/24.
+//
+
+import Foundation
---
+++ TravelTale/Views/Common/Place/PlaceDetailViewController.swift
@@ -0,0 +1,8 @@
+//
+//  PlaceDetailViewController.swift
+//  TravelTale
+//
+//  Created by 배지해 on 6/18/24.
+//
+
+import Foundation
