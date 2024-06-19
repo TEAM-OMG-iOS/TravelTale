e632d5153d625eb9dc4df3d8ee9ba35fb89bf031
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-06-19 16:38:26 +0900

    [Feat] #53 - (String+Extension) date 2개로 period string 만드는 함수 생성

--- TravelTale/Common/Extensions/String+Extension.swift
+++ TravelTale/Common/Extensions/String+Extension.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+extension String {
+    init(startDate: Date, endDate: Date) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy.MM.dd"
+
+        let startDateString = dateFormatter.string(from: startDate)
+        let endDateString = dateFormatter.string(from: endDate)
+
+        self = "\(startDateString) ~ \(endDateString)"
+    }
+}
