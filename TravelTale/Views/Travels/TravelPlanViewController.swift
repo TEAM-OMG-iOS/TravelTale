d9851b59954ca5463e987a305658454669fb0e9e
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-06-19 21:18:00 +0900

    [Feat] #53 - TravelViewModel 삭제, 데이터 바인딩 부분 주석처리

--- TravelTale/Views/Travels/TravelPlanViewController.swift
+++ TravelTale/Views/Travels/TravelPlanViewController.swift
@@ -11,7 +11,17 @@ final class TravelPlanViewController: BaseViewController {
     
     // MARK: - properties
     private let travelPlanView = TravelPlanView()
-    private let travelViewModel = TravelViewModel()
+    
+    // TODO: [String] -> [Travel]로 수정 필요
+    private var travels: [String] = [] {
+        didSet {
+//            splitTravels()
+            travelPlanView.tableView.reloadData()
+        }
+    }
+    
+    private var upcomingTravels: [Travel] = []
+    private var pastTravels: [Travel] = []
     
     // MARK: - life cycles
     override func loadView() {
@@ -20,42 +30,11 @@ final class TravelPlanViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addTemporaryData()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-    }
     
     // MARK: - methods
-    // TODO: 임시 travel 데이터 넣는 함수 (추후 삭제 예정)
-    private func addTemporaryData() {
-        travelViewModel.travelArray.value.append(contentsOf: [
-            Travel(
-                image: nil,
-                title: "200일 여행 with 남자친구",
-                startDate: createDate(year: 2024, month: 9, day: 19) ?? Date(),
-                endDate: createDate(year: 2024, month: 9, day: 21) ?? Date(),
-                province: "대구"),
-            Travel(
-                image: nil,
-                title: "24년의 가족 여행",
-                startDate: createDate(year: 2024, month: 4, day: 1) ?? Date(),
-                endDate: createDate(year: 2024, month: 4, day: 5) ?? Date(),
-                province: nil)
-        ])
-    }
-    
-    // TODO: date 데이터 만드는 함수 (추후 삭제 예정)
-    private func createDate(year: Int, month: Int, day: Int) -> Date? {
-        var dateComponents = DateComponents()
-        dateComponents.year = year
-        dateComponents.month = month
-        dateComponents.day = day
-        
-        return Calendar.current.date(from: dateComponents)
-    }
-    
+    // TODO: travels 데이터 추가
     override func configureStyle() {
         travelPlanView.tableView.separatorStyle = .none
         travelPlanView.tableView.sectionHeaderTopPadding = 0
@@ -75,18 +54,27 @@ final class TravelPlanViewController: BaseViewController {
         travelPlanView.addButtonView.button.addTarget(self, action: #selector(tappedAddButton), for: .touchUpInside)
     }
     
-    override func bind() {
-        travelViewModel.travelArray.bind { _ in
-            self.travelViewModel.splitTravelArray()
-            self.travelPlanView.tableView.reloadData()
-        }
-    }
+//    private func splitTravels() {
+//        let today = Date()
+//        upcomingTravels.removeAll()
+//        pastTravels.removeAll()
+//        
+//        for travel in travels {
+//            if travel.endDate >= today {
+//                upcomingTravels.append(travel)
+//            } else {
+//                pastTravels.append(travel)
+//            }
+//        }
+//    }
     
+    // MARK: - objc method
     @objc func tappedAddButton() {
         let TravelAddTitleVC = TravelAddTitleViewController()
         self.navigationController?.pushViewController(TravelAddTitleVC, animated: true)
     }
 }
+
 
 // MARK: - Extensions
 extension TravelPlanViewController: UITableViewDataSource {
@@ -98,9 +86,9 @@ extension TravelPlanViewController: UITableViewDataSource {
     // row cell
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if section == 0 {
-            return travelViewModel.upcomingTravels.count
+            return upcomingTravels.count
         } else {
-            return travelViewModel.pastTravels.count
+            return pastTravels.count
         }
     }
     
@@ -109,17 +97,12 @@ extension TravelPlanViewController: UITableViewDataSource {
         
         let travel: Travel
         if indexPath.section == 0 {
-            travel = travelViewModel.upcomingTravels[indexPath.row]
+            travel = upcomingTravels[indexPath.row]
         } else {
-            travel = travelViewModel.pastTravels[indexPath.row]
+            travel = pastTravels[indexPath.row]
         }
         
-        let period = travelViewModel.returnPeriodString(
-            startDate: travel.startDate,
-            endDate: travel.endDate
-        )
 //        cell.bind(travel: travel)
-        cell.hideThumbnail()
         cell.selectionStyle = .none
         
         return cell
@@ -128,16 +111,16 @@ extension TravelPlanViewController: UITableViewDataSource {
 
 extension TravelPlanViewController: UITableViewDelegate {
     // cell 선택 시
-     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-         let nextVC = TravelDetailViewController()
-         navigationController?.pushViewController(nextVC, animated: true)
-         
-         DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-             tableView.deselectRow(at: indexPath, animated: true)
-         }
-     }
-     
-     // Header, Footer
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let nextVC = TravelDetailViewController()
+        navigationController?.pushViewController(nextVC, animated: true)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+    }
+    
+    // Header, Footer
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: TravelSectionHeaderView.identifier) as? TravelSectionHeaderView else { return UIView() }
         
