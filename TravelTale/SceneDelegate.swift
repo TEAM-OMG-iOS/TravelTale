82ddbb4c617ac185905cddce5af9401d45a5dd6c
yoonhee <parkyunhee1217@gmail.com>
2024-06-19 11:26:38 +0900

    [Fix] (SceneDelegate) 원상복구

--- TravelTale/SceneDelegate.swift
+++ TravelTale/SceneDelegate.swift
@@ -22,7 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.rootViewController = TabBarViewController()
         window?.makeKeyAndVisible()
     }
-           
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
