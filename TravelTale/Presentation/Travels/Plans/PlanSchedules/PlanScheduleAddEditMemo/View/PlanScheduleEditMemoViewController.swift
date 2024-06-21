42a6c55cee088a39808e1cd434c517c84c665d87
Bread-kn72 <ordinarytom72@gmail.com>
2024-06-21 11:00:06 +0900

    [Add] #59 (PlanScheduleEditMemoViewController) 추가

---
+++ TravelTale/Presentation/Travels/Plans/PlanSchedules/PlanScheduleAddEditMemo/View/PlanScheduleEditMemoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlanScheduleEditMemoViewController.swift
+//  TravelTale
+//
+//  Created by Kinam on 6/21/24.
+//
+
+import UIKit
+
+class PlanScheduleEditMemoViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
