b48cc57664a51da195356730b64df7cfe515dbbe
KJH <gh7052@gmail.com>
2024-06-21 23:04:25 +0900

    [Feat] #20 - (SearchView, SearchTableViewCell, SearchViewController) UI 구성 및 초기 기능 구성

--- TravelTale/Presentation/Common/Searchs/Search/View/SearchViewController.swift
+++ TravelTale/Presentation/Common/Searchs/Search/View/SearchViewController.swift
@@ -7,23 +7,91 @@
 
 import UIKit
 
-class SearchViewController: BaseViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+final class SearchViewController: BaseViewController {
+    
+    // MARK: - properties
+    private let searchView = SearchView()
+    
+    // MARK: - life cycles
+    override func loadView() {
+        view = searchView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
-    */
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.tabBarController?.tabBar.isHidden = false
+    }
+    
+    // MARK: - methods
+    override func configureStyle() {
+        configureNavigation()
+    }
+    
+    override func configureDelegate() {
+        searchView.searchBar.delegate = self
+        
+        searchView.tableView.delegate = self
+        searchView.tableView.dataSource = self
+        
+        searchView.tableView.register(SearchTableViewCell.self, forCellReuseIdentifier: SearchTableViewCell.identifier)
+    }
+    
+    override func configureAddTarget() {
+        searchView.backBarButtonItem.target = self
+        searchView.backBarButtonItem.action = #selector(tappedBackBarButtonItem)
+    }
+    
+    private func configureNavigation() {
+        navigationItem.titleView = searchView.titleLabel
+        navigationItem.leftBarButtonItem = searchView.backBarButtonItem
+    }
+    
+    @objc private func tappedBackBarButtonItem() {
+        navigationController?.popViewController(animated: true)
+    }
+}
 
+// MARK: - extensions
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let searchResultVC = SearchResultViewController()
+        searchResultVC.searchText = searchBar.text
+        searchBar.resignFirstResponder()
+        
+        navigationController?.pushViewController(searchResultVC, animated: true)
+    }
+}
+
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let searchResultVC = SearchResultViewController()
+        searchResultVC.searchText = "임시 데이터"
+        
+        navigationController?.pushViewController(searchResultVC, animated: true)
+    }
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SearchTableViewCell.identifier, for: indexPath) as? SearchTableViewCell else { return UITableViewCell() }
+        
+        cell.selectionStyle = .none
+        
+        return cell
+    }
 }
