15b7d297c565f85148daade8bcacea46897209ca
KJH <gh7052@gmail.com>
2024-06-21 23:31:39 +0900

    [Feat] #20 - (SearchResultTabTotalViewController, SearchResultTabTouristViewController, SearchResultTabRestaurantViewController, SearchResultTabAccommodationViewController, SearchResultTabEntertainmentViewController) 초기 기능 구성

--- TravelTale/Presentation/Common/Searchs/SearchResults/SearchResultTab/View/SearchResultTabEntertainmentViewController.swift
+++ TravelTale/Presentation/Common/Searchs/SearchResults/SearchResultTab/View/SearchResultTabEntertainmentViewController.swift
@@ -6,24 +6,56 @@
 //
 
 import UIKit
+import XLPagerTabStrip
 
-class SearchResultTabEntertainmentViewController: BaseViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+final class SearchResultTabEntertainmentViewController: BaseViewController {
+    
+    // MARK: - properties
+    private let searchResultTabView = SearchResultTabView()
+    
+    // MARK: - life cycles
+    override func loadView() {
+        view = searchResultTabView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
-    */
+    
+    // MARK: - methods
+    override func configureDelegate() {
+        searchResultTabView.tableView.delegate = self
+        searchResultTabView.tableView.dataSource = self
+        
+        searchResultTabView.tableView.register(SearchResultTabTableViewCell.self, forCellReuseIdentifier: SearchResultTabTableViewCell.identifier)
+    }
+}
 
+// MARK: - extensions
+extension SearchResultTabEntertainmentViewController: IndicatorInfoProvider {
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return IndicatorInfo(title: "놀거리")
+    }
+}
+
+extension SearchResultTabEntertainmentViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let placeDetailViewController = PlaceDetailViewController()
+        
+        navigationController?.pushViewController(placeDetailViewController, animated: true)
+    }
+}
+
+extension SearchResultTabEntertainmentViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SearchResultTabTableViewCell.identifier, for: indexPath) as? SearchResultTabTableViewCell else { return UITableViewCell() }
+        
+        cell.selectionStyle = .none
+        
+        return cell
+    }
 }
