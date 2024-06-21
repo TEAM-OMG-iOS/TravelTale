50054fbf9bbf48571cac8cc1256d4a33ce976541
KJH <gh7052@gmail.com>
2024-06-22 02:04:07 +0900

    [Feat] #20 - (UserDefaultsManager) 키워드 생성, 가져오기, 삭제, 전체삭제 메소드 추가

--- TravelTale/Data/PersistentStorages/UserDefaultsStorage/UserDefaultsManager.swift
+++ TravelTale/Data/PersistentStorages/UserDefaultsStorage/UserDefaultsManager.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+
+final class UserDefaultsManager {
+    
+    // MARK: - properties
+    static let shared = UserDefaultsManager()
+    private init() {}
+    
+    private let keywordKey = "keyword"
+    
+    // MARK: - methods
+    func createKeyword(keyword: String) -> [String] {
+        var keywords = fetchKeywords()
+        
+        if let index = keywords.firstIndex(of: keyword) {
+            keywords.remove(at: index)
+        }
+        
+        keywords.insert(keyword, at: 0)
+        
+        UserDefaults.standard.set(keywords, forKey: keywordKey)
+        
+        return fetchKeywords()
+    }
+    
+    func fetchKeywords() -> [String] {
+        if let keywords = UserDefaults.standard.array(forKey: keywordKey) as? [String] {
+            return keywords
+        } else {
+            return []
+        }
+    }
+    
+    func deleteKeyword(keyword: String) -> [String] {
+        var keywords = fetchKeywords()
+        
+        if let index = keywords.firstIndex(of: keyword) {
+            keywords.remove(at: index)
+        }
+        
+        UserDefaults.standard.set(keywords, forKey: keywordKey)
+        
+        return fetchKeywords()
+    }
+    
+    func deleteAllKeywords() -> [String] {
+        UserDefaults.standard.removeObject(forKey: keywordKey)
+        
+        return fetchKeywords()
+    }
+}
