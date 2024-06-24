c3f8ac05730a8ad909065edd509f858923d12b3e
Bread-kn72 <ordinarytom72@gmail.com>
2024-06-25 00:40:42 +0900

    [Design] #59 (PlanScheduleAddEditMemoView) 글씨크기 수정

--- TravelTale/Presentation/Travels/Plans/PlanSchedules/PlanScheduleAddEditMemo/View/PlanScheduleAddEditMemoView.swift
+++ TravelTale/Presentation/Travels/Plans/PlanSchedules/PlanScheduleAddEditMemo/View/PlanScheduleAddEditMemoView.swift
@@ -21,7 +21,7 @@ final class PlanScheduleAddEditMemoView: BaseView {
     }
     
     private let memoTitle = UILabel().then {
-        $0.configureLabel(font: .pretendard(size: 18, weight: .bold), text: "메모")
+        $0.configureLabel(font: .pretendard(size: 16, weight: .bold), text: "메모")
     }
     
     let memoTV = UITextView().then {
@@ -29,7 +29,7 @@ final class PlanScheduleAddEditMemoView: BaseView {
         $0.textAlignment = .natural
         $0.text = "메세지를 입력하세요"
         $0.textColor = .lightGray
-        $0.font = .pretendard(size: 18, weight: .regular)
+        $0.font = .pretendard(size: 16, weight: .regular)
     }
     
     let completeBtn = GreenButton().then {
