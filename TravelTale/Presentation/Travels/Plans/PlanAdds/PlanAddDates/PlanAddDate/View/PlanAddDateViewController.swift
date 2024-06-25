47d023ae22f9f025ed1936f36ed58fb72151aa67
yoonhee <parkyunhee1217@gmail.com>
2024-06-25 21:18:18 +0900

    [Add] #56 - (PlanAddDateViewController) 네비게이션 버튼 클릭 시 Alert창

--- TravelTale/Presentation/Travels/Plans/PlanAdds/PlanAddDates/PlanAddDate/View/PlanAddDateViewController.swift
+++ TravelTale/Presentation/Travels/Plans/PlanAdds/PlanAddDates/PlanAddDate/View/PlanAddDateViewController.swift
@@ -44,6 +44,26 @@ final class PlanAddDateViewController: BaseViewController {
         navigationItem.leftBarButtonItem = planAddDateView.backButton
     }
     
+    private func presentAlert() {
+        let alert = UIAlertController(title: "경고", message: """
+    작성중인 내용이 저장되지 않습니다.
+    이전으로 돌아가시겠습니까?
+    """, preferredStyle: .alert)
+        
+        let cancel = UIAlertAction(title: "취소", style: .destructive)
+        
+        let ok = UIAlertAction(title: "확인", style: .default) { [weak self] _ in
+            guard let self = self else { return }
+            
+            self.navigationController?.popToRootViewController(animated: true)
+        }
+        
+        alert.addAction(cancel)
+        alert.addAction(ok)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
     @objc private func tappedCancelButton() {
         self.navigationController?.popViewController(animated: false)
     }
@@ -54,6 +74,6 @@ final class PlanAddDateViewController: BaseViewController {
     }
     
     @objc private func tappedBackButton() {
-        self.navigationController?.popToRootViewController(animated: true)
+        presentAlert()
     }
 }
