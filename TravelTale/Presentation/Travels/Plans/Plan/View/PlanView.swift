b9a13a7f28a641bc447c5e3225927ab6d760fb24
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-06-26 02:01:07 +0900

    [Design] #94 - (PlanView) not found view 디자인 추가 및 on/off 함수 생성

--- TravelTale/Presentation/Travels/Plans/Plan/View/PlanView.swift
+++ TravelTale/Presentation/Travels/Plans/Plan/View/PlanView.swift
@@ -11,8 +11,26 @@ final class PlanView: BaseView {
     
     // MARK: - properties
     let addButtonView = LightGreenButton()
+    
     let tableView = UITableView(frame: CGRect.zero, style: .grouped).then {
         $0.backgroundColor = .white
+    }
+    
+    let notFoundStackView = UIStackView().then {
+        $0.configureView(color: .white)
+        $0.axis = .vertical
+        $0.spacing = 30
+    }
+    
+    let notFoundImageView = UIImageView().then {
+        $0.image = UIImage(named: "not_found_train")
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    let notFoundLabel = UILabel().then {
+        $0.configureLabel(color: .gray70,
+                          font: .pretendard(size: 18, weight: .medium),
+                          text: "기록된 계획이 없습니다.")
     }
     
     // MARK: - methods
@@ -23,11 +41,14 @@ final class PlanView: BaseView {
     
     override func configureHierarchy() {
         [addButtonView,
-         tableView].forEach { self.addSubview($0) }
+         tableView,
+         notFoundStackView].forEach { self.addSubview($0) }
+        
+        [notFoundImageView,
+         notFoundLabel].forEach{ notFoundStackView.addArrangedSubview($0) }
     }
     
     override func configureConstraints() {
-        
         let horizontalInset = 24
         
         addButtonView.snp.makeConstraints {
@@ -39,6 +60,25 @@ final class PlanView: BaseView {
             $0.top.equalTo(addButtonView.snp.bottom).offset(26)
             $0.horizontalEdges.equalToSuperview().inset(horizontalInset)
             $0.bottom.equalToSuperview()
+        }
+        
+        notFoundStackView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        
+        notFoundImageView.snp.makeConstraints {
+            $0.width.equalTo(192)
+            $0.height.equalTo(98)
+        }
+    }
+    
+    func isNotFound(_ isNotFound: Bool) {
+        if isNotFound {
+            tableView.isHidden = true
+            notFoundStackView.isHidden = false
+        } else {
+            tableView.isHidden = false
+            notFoundStackView.isHidden = true
         }
     }
 }
