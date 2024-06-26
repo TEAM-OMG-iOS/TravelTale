f2a819ae39d13aa5b2cc99b259dc8e3f1a409b0e
KJH <gh7052@gmail.com>
2024-06-26 17:54:35 +0900

    [Feat] #20 - (SearchResultTabView) 데이터가 없을 때의 화면인 NotFoundView UI 및 bind 메소드 추가

--- TravelTale/Presentation/Common/Searchs/SearchResults/SearchResultTab/View/SearchResultTabView.swift
+++ TravelTale/Presentation/Common/Searchs/SearchResults/SearchResultTab/View/SearchResultTabView.swift
@@ -12,14 +12,30 @@ final class SearchResultTabView: BaseView {
     // MARK: - properties
     let tableView = UITableView()
     
+    private let notFoundView = NotFoundView().then {
+        $0.setLabel(text: "검색 결과가 존재하지 않습니다.")
+    }
+    
     // MARK: - methods
     override func configureHierarchy() {
         self.addSubview(tableView)
+        self.addSubview(notFoundView)
     }
     
     override func configureConstraints() {
         tableView.snp.makeConstraints {
             $0.edges.equalToSuperview()
+        }
+        
+        notFoundView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    func bind(hasPlaces: Bool) {
+        if hasPlaces {
+            tableView.isHidden = false
+            notFoundView.isHidden = true
         }
     }
 }
