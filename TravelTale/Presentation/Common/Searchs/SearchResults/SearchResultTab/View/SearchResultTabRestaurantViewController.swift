44dab6f84518f85fbba04b22e19813faecec841e
KJH <gh7052@gmail.com>
2024-06-26 15:39:31 +0900

    [Feat] #20 - (SearchResultTabTotalViewController, SearchResultTabTouristViewController, SearchResultTabRestaurantViewController, SearchResultTabAccommodationViewController, SearchResultTabEntertainmentViewController) 데이터 바인딩 기능, 재검색 기능, 페이징 기능 추가

--- TravelTale/Presentation/Common/Searchs/SearchResults/SearchResultTab/View/SearchResultTabRestaurantViewController.swift
+++ TravelTale/Presentation/Common/Searchs/SearchResults/SearchResultTab/View/SearchResultTabRestaurantViewController.swift
@@ -13,7 +13,28 @@ final class SearchResultTabRestaurantViewController: BaseViewController {
     // MARK: - properties
     private let searchResultTabView = SearchResultTabView()
     
+    private let realmManager = RealmManager.shared
+    private let networkManager = NetworkManager.shared
+    
+    private let tabType = UserDefaultsManager.shared.fetchTabType()
+    private lazy var bookmarks = realmManager.fetchBookmarks(contentTypeId: .restaurant)
+    
+    private var page = 1
+    private var searchText: String
+    private var isLoadingLast = false
+    private var places: [PlaceSearch] = []
+    
     // MARK: - life cycles
+    init(searchText: String) {
+        self.searchText = searchText
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func loadView() {
         view = searchResultTabView
     }
@@ -28,6 +49,67 @@ final class SearchResultTabRestaurantViewController: BaseViewController {
         searchResultTabView.tableView.dataSource = self
         
         searchResultTabView.tableView.register(SearchResultTabTableViewCell.self, forCellReuseIdentifier: SearchResultTabTableViewCell.identifier)
+    }
+    
+    override func bind() {
+        switch tabType {
+        case .travel:
+            fetchSearchedPlacesByTravelTapType()
+        case .discovery:
+            fetchSearchedPlacesByDiscoveryTapType()
+        }
+    }
+    
+    private func fetchSearchedPlacesByTravelTapType() {
+        networkManager.fetchSearchedPlaces(keyword: searchText,
+                                           type: .restaurant,
+                                           page: page) { [weak self] result in
+            self?.fetchResult(result: result)
+        }
+    }
+    
+    private func fetchSearchedPlacesByDiscoveryTapType() {
+        if let region = realmManager.fetchRegion() {
+            networkManager.fetchSearchedPlaces(sidoCode: region.sidoCode,
+                                               sigunguCode: region.sigunguCode ?? "",
+                                               keyword: searchText,
+                                               type: .restaurant,
+                                               page: page) { [weak self] result in
+                self?.fetchResult(result: result)
+            }
+        } else {
+            fetchSearchedPlacesByTravelTapType()
+        }
+    }
+    
+    private func fetchResult(result: Result<PlaceSearchs, any Error>) {
+        switch result {
+        case .success(let data):
+            places += data.placeSearchs ?? []
+            searchResultTabView.tableView.reloadData()
+        case .failure(_):
+            isLoadingLast = true
+        }
+    }
+    
+    func bindRefetchedPlaces(searchText: String) {
+        self.searchText = searchText
+        
+        places = []
+        isLoadingLast = false
+        page = 1
+        
+        bind()
+    }
+    
+    private func bindMorePlaces() {
+        if isLoadingLast == true {
+            return
+        }
+        
+        page += 1
+        
+        bind()
     }
 }
 
@@ -44,17 +126,26 @@ extension SearchResultTabRestaurantViewController: UITableViewDelegate {
         
         navigationController?.pushViewController(placeDetailViewController, animated: true)
     }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row == places.count - 1 {
+            bindMorePlaces()
+        }
+    }
 }
 
 extension SearchResultTabRestaurantViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return places.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: SearchResultTabTableViewCell.identifier, for: indexPath) as? SearchResultTabTableViewCell else { return UITableViewCell() }
         
+        let isBookmarked = bookmarks.map { $0.contentId }.contains(places[indexPath.row].contentId)
+        
         cell.selectionStyle = .none
+        cell.bind(place: places[indexPath.row], isBookmarked: isBookmarked)
         
         return cell
     }
