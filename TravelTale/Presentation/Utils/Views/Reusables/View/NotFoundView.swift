88cc36199aaa7f0fd02ab0e64e2889bf35e47285
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-06-26 12:13:21 +0900

    [Design] #94 - (NotFoundView) 레이아웃 조정

--- TravelTale/Presentation/Utils/Views/Reusables/View/NotFoundView.swift
+++ TravelTale/Presentation/Utils/Views/Reusables/View/NotFoundView.swift
@@ -10,18 +10,18 @@ import UIKit
 class NotFoundView: BaseView {
     
     // MARK: - properties
-    let notFoundStackView = UIStackView().then {
+    let stackView = UIStackView().then {
         $0.configureView(color: .white)
         $0.axis = .vertical
         $0.spacing = 30
     }
     
-    let notFoundImageView = UIImageView().then {
+    let imageView = UIImageView().then {
         $0.image = UIImage(named: "not_found_train")
         $0.contentMode = .scaleAspectFit
     }
     
-    let notFoundLabel = UILabel().then {
+    let label = UILabel().then {
         $0.configureLabel(alignment: .center,
                           color: .gray70,
                           font: .pretendard(size: 18, weight: .medium),
@@ -30,28 +30,28 @@ class NotFoundView: BaseView {
     
     // MARK: - methods
     override func configureHierarchy() {
-        self.addSubview(notFoundStackView)
+        self.addSubview(stackView)
         
-        [notFoundImageView,
-         notFoundLabel].forEach{ notFoundStackView.addArrangedSubview($0) }
+        [imageView,
+         label].forEach{ stackView.addArrangedSubview($0) }
     }
     
     override func configureConstraints() {
-        notFoundStackView.snp.makeConstraints {
+        stackView.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.bottom.equalTo(self.snp.centerY).offset(10)
+            $0.centerY.equalTo(self.snp.centerY).offset(-30)
         }
         
-        notFoundImageView.snp.makeConstraints {
+        imageView.snp.makeConstraints {
             $0.height.equalTo(98)
         }
         
-        notFoundLabel.snp.makeConstraints {
+        label.snp.makeConstraints {
             $0.width.equalTo(250)
         }
     }
     
     func setLabel(text: String) {
-        notFoundLabel.text = text
+        label.text = text
     }
 }
