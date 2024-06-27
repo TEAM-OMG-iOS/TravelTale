636fc7aa77baa08ae9735f695da199bf2d07ef76
Bread-kn72 <ordinarytom72@gmail.com>
2024-06-27 18:05:34 +0900

    [Feat] #58 (PlanScheduleAddEditPlaceView) 크래시 방지용 더미 추가

--- TravelTale/Presentation/Travels/Plans/PlanSchedules/PlanScheduleAddEditPlace/View/PlanScheduleAddEditPlaceView.swift
+++ TravelTale/Presentation/Travels/Plans/PlanSchedules/PlanScheduleAddEditPlace/View/PlanScheduleAddEditPlaceView.swift
@@ -96,6 +96,8 @@ class PlanScheduleAddEditPlaceView: BaseView {
         $0.configureButton(fontColor: .white, font: .pretendard(size: 18, weight: .semibold), text: "완료")
     }
     
+    let dummyPlaceDetail = PlaceDetail(contentId: "", contentTypeId: "", title: "No PlaceDetail", tel: "", homepage: "", firstImage: "", firstImage2: "", cpyrhtDivCd: "", addr1: "", addr2: "", mapx: "", mapy: "", overview: "")
+    
     // MARK: - methods
     override func configureHierarchy() {
         [placeView, scheduleView, startTimeView, memoView ,completedBtn].forEach {
