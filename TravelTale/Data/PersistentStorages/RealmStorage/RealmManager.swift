39b34aad53aa5c1e7fa5a1268a357e0211608162
KJH <gh7052@gmail.com>
2024-07-01 03:14:35 +0900

    [Refactor] #77 - (RealmManager) updateTravel 메서드에 날짜 범위 변환 및 초과 날짜 삭제 기능 추가

--- TravelTale/Data/PersistentStorages/RealmStorage/RealmManager.swift
+++ TravelTale/Data/PersistentStorages/RealmStorage/RealmManager.swift
@@ -110,6 +110,8 @@ final class RealmManager {
                     travel.endDate = endDate
                 }
             }
+            
+            updateDateRange(travel: travel)
         } catch {
             print("updateTravel 실패")
         }
@@ -344,6 +346,48 @@ final class RealmManager {
             }
         } catch {
             print("deleteBookmark 실패")
+        }
+    }
+}
+
+// MARK: - extensions
+extension RealmManager {
+    private func updateDateRange(travel: Travel) {
+        let startDate = travel.startDate
+        let endDate = travel.endDate
+        
+        let calendar = Calendar.current
+        
+        let endDay = calendar.dateComponents([.day], from: endDate).day
+        let startDay = calendar.dateComponents([.day], from: startDate).day
+        
+        for schedule in travel.schedules {
+            if let day = schedule.day, let scheduleDay = Int(day), let endDay, let startDay {
+                if scheduleDay > endDay - startDay + 1 {
+                    deleteSchedule(travel: travel, schedule: schedule)
+                } else {
+                    if let oldDate = schedule.date {
+                        let oldDateComponents = calendar.dateComponents([.hour, .minute, .second], from: oldDate)
+                        var newDateComponents = calendar.dateComponents([.year, .month, .day], from: startDate)
+                        
+                        newDateComponents.hour = oldDateComponents.hour
+                        newDateComponents.minute = oldDateComponents.minute
+                        newDateComponents.second = oldDateComponents.second
+                        
+                        if let newDateComponentsDay = newDateComponents.day, let day = Int(day) {
+                            newDateComponents.day = newDateComponentsDay + day - 1
+                        }
+
+                        do {
+                            try realm.write {
+                                schedule.date = calendar.date(from: newDateComponents) ?? Date()
+                            }
+                        } catch {
+                            print("updateDateRange 실패")
+                        }
+                    }
+                }
+            }
         }
     }
 }
